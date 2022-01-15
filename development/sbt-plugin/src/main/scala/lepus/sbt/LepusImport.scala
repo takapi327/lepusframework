7a3559bb5dce3300804e95c45d8662358c10e802
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-01-16 00:27:36 +0900

    Create LepusImport

---
+++ development/sbt-plugin/src/main/scala/lepus/sbt/LepusImport.scala
@@ -0,0 +1,34 @@
+/**
+ *  This file is part of the Lepus Framework.
+ *  For the full copyright and license information,
+ *  please view the LICENSE file that was distributed with this source code.
+ */
+
+package lepus.sbt
+
+import sbt._
+
+object LepusImport {
+
+  def component(id: String) = "com.github.takapi327" %% id % lepus.core.LepusVersion.current
+
+  val lepusCore = component("lepus")
+
+  object LepusKeys {
+
+    val routePackage = TaskKey[String](
+      label       = "routePackage",
+      description = "Package of server routing information"
+    )
+
+    val defaultPort = SettingKey[Int](
+      label       = "defaultPort",
+      description = "The default port that Server runs on"
+    )
+
+    val defaultAddress = SettingKey[String](
+      label       = "defaultAddress",
+      description = "The default address that Server runs on"
+    )
+  }
+}
