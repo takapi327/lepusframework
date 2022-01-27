e69611c7804b1fe0b160281c0baeda958d3eb151
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-01-28 00:16:58 +0900

    Create louter package object in lepus-router project

---
+++ core/lepus-router/src/main/scala/lepus/router/package.scala
@@ -0,0 +1,9 @@
+/**
+ *  This file is part of the Lepus Framework.
+ *  For the full copyright and license information,
+ *  please view the LICENSE file that was distributed with this source code.
+ */
+
+package lepus
+
+package object router extends LepusRouter with ExtensionMethods
