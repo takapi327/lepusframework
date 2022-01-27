6c911b7c64b84642593037a8e28631159608e167
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-01-28 00:14:28 +0900

    Create RequestEndpoint in lepus-router project

---
+++ core/lepus-router/src/main/scala/lepus/router/http/RequestEndpoint.scala
@@ -0,0 +1,27 @@
+/**
+ *  This file is part of the Lepus Framework.
+ *  For the full copyright and license information,
+ *  please view the LICENSE file that was distributed with this source code.
+ */
+
+package lepus.router.http
+
+sealed trait RequestEndpoint[T] {
+
+  def and[T](other: RequestEndpoint[T]): RequestEndpoint[T] =
+    RequestEndpoint.Pair(this, other)
+  def /[T](other: RequestEndpoint[T]): RequestEndpoint[T] = and(other)
+}
+
+object RequestEndpoint {
+
+  case class Method[T](method: RequestMethod, converter: EndpointConverter[Unit, T]) extends RequestEndpoint[T]
+  case class FixedPath[T](name: String, converter: EndpointConverter[String, T]) extends RequestEndpoint[T]
+  case class AnyPath[T](name: Option[String], converter: EndpointConverter[String, T]) extends RequestEndpoint[T]
+  case class QueryParam[T](name: String, converter: EndpointConverter[String, T]) extends RequestEndpoint[T]
+
+  case class Pair[L, R, LR](
+    left:  RequestEndpoint[L],
+    right: RequestEndpoint[R]
+  ) extends RequestEndpoint[LR]
+}
