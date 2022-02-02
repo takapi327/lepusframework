7a3e68bf291f6d46c70be5bb5bbaf42c011cdf76
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-02-02 22:16:00 +0900

    Create DecodeResult lepus-router project

---
+++ core/lepus-router/src/main/scala/lepus/router/http/DecodeResult.scala
@@ -0,0 +1,17 @@
+/**
+ *  This file is part of the Lepus Framework.
+ *  For the full copyright and license information,
+ *  please view the LICENSE file that was distributed with this source code.
+ */
+
+package lepus.router.http
+
+sealed trait DecodeResult[+T]
+object DecodeResult {
+  case class Success[T](value: T) extends DecodeResult[T]
+
+  sealed trait Failure extends DecodeResult[Nothing]
+  case class InvalidValue(value: String, throwable: Throwable) extends Failure
+  case class Mismatch(request: String, endpoint: String) extends Failure
+  case object Missing extends Failure
+}
