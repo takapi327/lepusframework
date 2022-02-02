82543f1f2ac06d9fa1c4f55d9215b1d09cc4a341
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-02-03 00:01:36 +0900

    Add routesSemigroup implicit val in package

--- core/lepus-router/src/main/scala/lepus/router/package.scala
+++ core/lepus-router/src/main/scala/lepus/router/package.scala
@@ -6,4 +6,13 @@
 
 package lepus
 
-package object router extends LepusRouter with ExtensionMethods
+import cats.Semigroup
+import cats.syntax.semigroupk._
+
+import cats.effect.IO
+
+import org.http4s.HttpRoutes
+
+package object router extends LepusRouter with ExtensionMethods {
+  implicit val routesSemigroup: Semigroup[HttpRoutes[IO]] = _ combineK _
+}
