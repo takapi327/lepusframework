1d7112df2c8c5c12bcc170e251658836dd1a7965
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-02-08 00:31:37 +0900

    Create RequestDecodeHandler in lepus-router project

---
+++ core/lepus-router/src/main/scala/lepus/router/RequestDecodeHandler.scala
@@ -0,0 +1,21 @@
+/**
+ *  This file is part of the Lepus Framework.
+ *  For the full copyright and license information,
+ *  please view the LICENSE file that was distributed with this source code.
+ */
+
+package lepus.router
+
+object RequestDecodeHandler {
+
+  def handleRequest[F[_], T](
+    serverRequest: ServerRequest[F],
+    endpoint:      LepusEndpoint[F, _, T]
+  ): DecodeRequestResult = {
+    val (decodeEndpointResult, _) = DecodeEndpoint(serverRequest, endpoint.endpoint)
+    decodeEndpointResult match {
+      case _: DecodeEndpointResult.Failure      => DecodeRequestResult.Failure()
+      case DecodeEndpointResult.Success(values) => DecodeRequestResult.Success(values)
+    }
+  }
+}
