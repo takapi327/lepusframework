ac5cead6948dd6195894b84b4fa522832462913b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-02-07 23:51:53 +0900

    Fix BodyConverter in lepus-router project

--- core/lepus-router/src/main/scala/lepus/router/http/BodyConverter.scala
+++ core/lepus-router/src/main/scala/lepus/router/http/BodyConverter.scala
@@ -6,31 +6,35 @@
 
 package lepus.router.http
 
+import java.nio.charset.StandardCharsets._
+
 import cats.data.Validated
 
 import io.circe.{ Encoder, Decoder, ParsingFailure, DecodingFailure, CursorOp, Printer, Errors }
 import io.circe.syntax._
 import io.circe.parser.decodeAccumulating
 
+import fs2._
+
 import ConvertResult._
 
 trait BodyConverter[T] {
   def decode(s: String): ConvertResult
-  def encode(t: T):      String
+  def encode(t: T):      ConvertResult
 }
 
 object BodyConverter {
 
   case class Json[T](
     private val _decode: String => ConvertResult
-  )(_encode: T => String) extends BodyConverter[T] {
+  )(_encode: T => ConvertResult) extends BodyConverter[T] {
     override def decode(s: String): ConvertResult = _decode(s)
-    override def encode(t: T):      String        = _encode(t)
+    override def encode(t: T):      ConvertResult = _encode(t)
   }
 
   object Json {
     def toJson[T: Encoder: Decoder](s: String): ConvertResult = circeJson[T].decode(s)
-    def toJson[T: Encoder: Decoder](t: T):      String        = circeJson[T].encode(t)
+    def toJson[T: Encoder: Decoder](t: T):      ConvertResult = circeJson[T].encode(t)
   }
 
   implicit def circeJson[T: Encoder: Decoder]: Json[T] =
@@ -38,7 +42,7 @@ object BodyConverter {
 
   implicit def decodeCirceJson[T: Encoder: Decoder](s: String): ConvertResult =
     decodeAccumulating[T](s) match {
-      case Validated.Valid(value) => Success(value)
+      case Validated.Valid(value) => JsValue(value)
       case Validated.Invalid(circeFailures) =>
         val jsonFailures = circeFailures.map {
           case ParsingFailure(message, _) => Error.JsonError(message, List.empty)
@@ -50,17 +54,32 @@ object BodyConverter {
         Error(s, Error.JsonDecodeException(jsonFailures.toList, Errors(circeFailures)))
     }
 
-  implicit def encodeCirceJson[T: Encoder: Decoder](t: T): String =
-    Printer.noSpaces.print(t.asJson)
+  implicit def encodeCirceJson[T: Encoder: Decoder](t: T): ConvertResult =
+    JsValue(Printer.noSpaces.print(t.asJson))
 }
 
-trait ConvertResult
+trait ConvertResult {
+  def toStream(): Stream[Pure, Byte]
+}
 
 object ConvertResult {
-  case class Success[T](value: T) extends ConvertResult
+  sealed trait Success extends ConvertResult
   sealed trait Failure extends ConvertResult
 
-  case class Error(message: String, throwable: Throwable) extends Failure
+  case class JsValue[T](value: T) extends Success {
+    override def toStream(): Stream[Pure, Byte] = {
+      val bytes = value.toString.getBytes(UTF_8)
+      Stream.chunk(Chunk.array(bytes))
+    }
+  }
+  object JsValue
+
+  case class Error(message: String, throwable: Throwable) extends Failure {
+    override def toStream(): Stream[Pure, Byte] = {
+      val bytes = message.getBytes(UTF_8)
+      Stream.chunk(Chunk.array(bytes))
+    }
+  }
   object Error {
     case class JsonDecodeException(errors: List[JsonError], underlying: Throwable)
       extends Exception(underlying.getMessage, underlying, true, false)
