da692b3781b705ca0c0dbe81ab2dba5f9133dcb6
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-02-26 17:33:53 +0900

    Add lepusGenerateSwagger method in LepusGenerator

--- development/sbt-plugin/src/main/scala/lepus/sbt/LepusGenerator.scala
+++ development/sbt-plugin/src/main/scala/lepus/sbt/LepusGenerator.scala
@@ -6,6 +6,14 @@
 
 package lepus.sbt
 
+/**
+ * reflective access of structural type member method run should be enabled by making the implicit value scala.language.
+ * reflectiveCalls visible.
+ * This can be achieved by adding the import clause 'import scala.language.reflectiveCalls' or by setting the compiler option -language:reflectiveCalls.
+ * See the Scaladoc for value scala.language.reflectiveCalls for a discussion why the feature should be explicitly enabled.
+ */
+import scala.language.reflectiveCalls
+
 import sbt._
 import sbt.Keys._
 
@@ -14,10 +22,23 @@ import LepusImport.LepusKeys._
 
 object LepusGenerator {
 
+  private lazy val projectClassLoader = new ProjectClassLoader(
+    urls   = convertToUrls(lepusDependencyClasspath.value.files),
+    parent = baseClassloader.value
+  )
+
   val generateServer = lepusGenerateServer(
     mainClassName = (Compile / run / mainClass),
     host          = defaultAddress,
     port          = defaultPort,
+    routePackage  = routePackage,
+    generatedDir  = (Compile / sourceManaged)
+  )
+
+  val generateSwagger = lepusGenerateSwagger(
+    mainClassName = (Compile / run / mainClass),
+    title         = swaggerTitle,
+    version       = swaggerVersion,
     routePackage  = routePackage,
     generatedDir  = (Compile / sourceManaged)
   )
@@ -41,20 +62,7 @@ object LepusGenerator {
       ): File
     }
 
-    lazy val projectClassLoader = new ProjectClassLoader(
-      urls   = convertToUrls(lepusDependencyClasspath.value.files),
-      parent = baseClassloader.value
-    )
-
-    /**
-     * reflective access of structural type member method run should be enabled by making the implicit value scala.language.
-     * reflectiveCalls visible.
-     * This can be achieved by adding the import clause 'import scala.language.reflectiveCalls' or by setting the compiler option -language:reflectiveCalls.
-     * See the Scaladoc for value scala.language.reflectiveCalls for a discussion why the feature should be explicitly enabled.
-     */
-    import scala.language.reflectiveCalls
-    val mainClass:  Class[_] = projectClassLoader.loadClass(mainClassName.value.getOrElse("lepus.server.LepusServer") + "$")
-    val mainObject: Server   = mainClass.getField("MODULE$").get(null).asInstanceOf[Server]
+    val mainObject: Server = loadFromClassName(mainClassName, "lepus.server.LepusServer")
 
     Seq(mainObject.generate(
       host         = host.value,
@@ -62,5 +70,40 @@ object LepusGenerator {
       routePackage = routePackage.value,
       generatedDir = generatedDir.value
     ))
+  }
+
+  def lepusGenerateSwagger(
+    mainClassName: TaskKey[Option[String]],
+    title:         SettingKey[String],
+    version:       SettingKey[String],
+    routePackage:  TaskKey[String],
+    generatedDir:  SettingKey[File]
+  ): Def.Initialize[Task[Seq[File]]] = Def.task {
+
+    type Swagger = {
+      def generateSwagger(
+        title:        String,
+        version:      String,
+        routePackage: String,
+        generatedDir: File
+      ): File
+    }
+
+    val mainObject: Swagger = loadFromClassName(mainClassName, "lepus.swagger.Generator")
+
+    Seq(mainObject.generateSwagger(
+      title        = title.value,
+      version      = version.value,
+      routePackage = routePackage.value,
+      generatedDir = generatedDir.value
+    ))
+  }
+
+  private def loadFromClassName[T](
+    mainClassName: TaskKey[Option[String]],
+    className:     String
+  ): T = {
+    val mainClass: Class[_] = projectClassLoader.loadClass(mainClassName.value.getOrElse(className) + "$")
+    mainClass.getField("MODULE$").get(null).asInstanceOf[T]
   }
 }
