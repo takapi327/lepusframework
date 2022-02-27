99da0e433f7588399606e5a9ff67b6cfef48c7ae
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-02-27 16:53:22 +0900

    Delete unused Lepus Server Generator

--- development/sbt-plugin/src/main/scala/lepus/sbt/LepusGenerator.scala
+++ development/sbt-plugin/src/main/scala/lepus/sbt/LepusGenerator.scala
@@ -23,14 +23,6 @@ import LepusSwaggerImport._
 
 object LepusGenerator {
 
-  val generateServer = lepusGenerateServer(
-    mainClassName = (Compile / run / mainClass),
-    host          = defaultAddress,
-    port          = defaultPort,
-    routePackage  = routePackage,
-    generatedDir  = (Compile / sourceManaged)
-  )
-
   val generateSwagger = lepusGenerateSwagger(
     mainClassName = (Compile / run / mainClass),
     title         = swaggerTitle,
@@ -40,39 +32,6 @@ object LepusGenerator {
   )
 
   def convertToUrls(files: Seq[File]): Array[URL] = files.map(_.toURI.toURL).toArray
-
-  def lepusGenerateServer(
-    mainClassName: TaskKey[Option[String]],
-    host:          SettingKey[String],
-    port:          SettingKey[Int],
-    routePackage:  TaskKey[String],
-    generatedDir:  SettingKey[File]
-  ): Def.Initialize[Task[Seq[File]]] = Def.task {
-
-    type Server = {
-      def generate(
-        host:         String,
-        port:         Int,
-        routePackage: String,
-        generatedDir: File
-      ): File
-    }
-
-    lazy val projectClassLoader = new ProjectClassLoader(
-      urls   = convertToUrls(lepusDependencyClasspath.value.files),
-      parent = baseClassloader.value
-    )
-
-    val mainClass: Class[_] = projectClassLoader.loadClass(mainClassName.value.getOrElse("lepus.server.LepusServer") + "$")
-    val mainObject: Server = mainClass.getField("MODULE$").get(null).asInstanceOf[Server]
-
-    Seq(mainObject.generate(
-      host         = host.value,
-      port         = port.value,
-      routePackage = routePackage.value,
-      generatedDir = generatedDir.value
-    ))
-  }
 
   def lepusGenerateSwagger(
     mainClassName: TaskKey[Option[String]],
