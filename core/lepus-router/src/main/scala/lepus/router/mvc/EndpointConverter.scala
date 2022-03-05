e7241e3c50d89e016fe36b96421a726dd6b6d029
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-03-05 18:08:27 +0900

    Fix EndpointConverter

--- core/lepus-router/src/main/scala/lepus/router/mvc/EndpointConverter.scala
+++ core/lepus-router/src/main/scala/lepus/router/mvc/EndpointConverter.scala
@@ -24,7 +24,7 @@ trait EndpointConverter[L, H] {
   def decode(str: L, fromTo: L => DecodeResult[H]): DecodeResult[H] = {
     Try { fromTo(str) } match {
       case Success(s) => s
-      case Failure(e) => DecodeResult.InvalidValue(str.toString, e)
+      case Failure(e) => DecodeResult.InvalidValue(str.toString, Some(e))
     }
   }
 
@@ -38,70 +38,77 @@ object EndpointConverter {
     override def schema: Schema = Schema.string
   }
   implicit val byte: EndpointConverter[String, Byte] = new EndpointConverter[String, Byte] {
-    override def stringToResult(str: String): DecodeResult[Byte] = DecodeResult.Success(str.toByte)
+    override def stringToResult(str: String): DecodeResult[Byte] = stringTo(_.toByte)(str)
     override def schema: Schema = Schema.byteString
   }
   implicit val short: EndpointConverter[String, Short] = new EndpointConverter[String, Short] {
-    override def stringToResult(str: String): DecodeResult[Short] = DecodeResult.Success(str.toShort)
+    override def stringToResult(str: String): DecodeResult[Short] = stringTo(_.toShort)(str)
     override def schema: Schema = Schema.int32Integer
   }
   implicit val int: EndpointConverter[String, Int] = new EndpointConverter[String, Int] {
-    override def stringToResult(str: String): DecodeResult[Int] = DecodeResult.Success(str.toInt)
+    override def stringToResult(str: String): DecodeResult[Int] = stringTo(_.toInt)(str)
     override def schema: Schema = Schema.int32Integer
   }
   implicit val long: EndpointConverter[String, Long] = new EndpointConverter[String, Long] {
-    override def stringToResult(str: String): DecodeResult[Long] = DecodeResult.Success(str.toLong)
+    override def stringToResult(str: String): DecodeResult[Long] = stringTo(_.toLong)(str)
     override def schema: Schema = Schema.int64Integer
   }
   implicit val float: EndpointConverter[String, Float] = new EndpointConverter[String, Float] {
-    override def stringToResult(str: String): DecodeResult[Float] = DecodeResult.Success(str.toFloat)
+    override def stringToResult(str: String): DecodeResult[Float] = stringTo(_.toFloat)(str)
     override def schema: Schema = Schema.floatNumber
   }
   implicit val double: EndpointConverter[String, Double] = new EndpointConverter[String, Double] {
-    override def stringToResult(str: String): DecodeResult[Double] = DecodeResult.Success(str.toDouble)
+    override def stringToResult(str: String): DecodeResult[Double] = stringTo(_.toDouble)(str)
     override def schema: Schema = Schema.doubleNumber
   }
   implicit val boolean: EndpointConverter[String, Boolean] = new EndpointConverter[String, Boolean] {
-    override def stringToResult(str: String): DecodeResult[Boolean] = DecodeResult.Success(str.toBoolean)
+    override def stringToResult(str: String): DecodeResult[Boolean] = stringTo(_.toBoolean)(str)
     override def schema: Schema = Schema.boolean
   }
   implicit val bigDecimal: EndpointConverter[String, BigDecimal] = new EndpointConverter[String, BigDecimal] {
-    override def stringToResult(str: String): DecodeResult[BigDecimal] = DecodeResult.Success(BigDecimal(str))
+    override def stringToResult(str: String): DecodeResult[BigDecimal] = stringTo(BigDecimal(_))(str)
     override def schema: Schema = Schema.string
   }
   implicit val localTime: EndpointConverter[String, LocalTime] = new EndpointConverter[String, LocalTime] {
-    override def stringToResult(str: String): DecodeResult[LocalTime] = DecodeResult.Success(LocalTime.parse(str))
+    override def stringToResult(str: String): DecodeResult[LocalTime] = stringTo(LocalTime.parse(_))(str)
     override def schema: Schema = Schema.string
   }
   implicit val localDate: EndpointConverter[String, LocalDate] = new EndpointConverter[String, LocalDate] {
-    override def stringToResult(str: String): DecodeResult[LocalDate] = DecodeResult.Success(LocalDate.parse(str))
+    override def stringToResult(str: String): DecodeResult[LocalDate] = stringTo(LocalDate.parse(_))(str)
     override def schema: Schema = Schema.dateString
   }
   implicit val localDateTime: EndpointConverter[String, LocalDateTime] = new EndpointConverter[String, LocalDateTime] {
-    override def stringToResult(str: String): DecodeResult[LocalDateTime] = DecodeResult.Success(LocalDateTime.parse(str))
+    override def stringToResult(str: String): DecodeResult[LocalDateTime] = stringTo(LocalDateTime.parse(_))(str)
     override def schema: Schema = Schema.string
   }
   implicit val offsetTime: EndpointConverter[String, OffsetTime] = new EndpointConverter[String, OffsetTime] {
-    override def stringToResult(str: String): DecodeResult[OffsetTime] = DecodeResult.Success(OffsetTime.parse(str))
+    override def stringToResult(str: String): DecodeResult[OffsetTime] = stringTo(OffsetTime.parse(_))(str)
     override def schema: Schema = Schema.string
   }
   implicit val offsetDateTime: EndpointConverter[String, OffsetDateTime] = new EndpointConverter[String, OffsetDateTime] {
-    override def stringToResult(str: String): DecodeResult[OffsetDateTime] = DecodeResult.Success(OffsetDateTime.parse(str))
+    override def stringToResult(str: String): DecodeResult[OffsetDateTime] = stringTo(OffsetDateTime.parse(_))(str)
     override def schema: Schema = Schema.dateTimeString
   }
   implicit val zonedDateTime: EndpointConverter[String, ZonedDateTime] = new EndpointConverter[String, ZonedDateTime] {
-    override def stringToResult(str: String): DecodeResult[ZonedDateTime] = DecodeResult.Success(ZonedDateTime.parse(str))
+    override def stringToResult(str: String): DecodeResult[ZonedDateTime] = stringTo(ZonedDateTime.parse(_))(str)
     override def schema: Schema = Schema.dateTimeString
   }
   implicit val instant: EndpointConverter[String, Instant] = new EndpointConverter[String, Instant] {
-    override def stringToResult(str: String): DecodeResult[Instant] = DecodeResult.Success(Instant.parse(str))
+    override def stringToResult(str: String): DecodeResult[Instant] = stringTo(Instant.parse(_))(str)
     override def schema: Schema = Schema.dateTimeString
   }
   implicit val uuid: EndpointConverter[String, UUID] = new EndpointConverter[String, UUID] {
-    override def stringToResult(str: String): DecodeResult[UUID] = DecodeResult.Success(UUID.fromString(str))
+    override def stringToResult(str: String): DecodeResult[UUID] = stringTo(UUID.fromString(_))(str)
     override def schema: Schema = Schema.uuidString
   }
 
+  /**
+   * Converted from String to any type and finally to DecodeResult
+   *
+   * @param fromTo Convert from String to type T
+   * @tparam T Type of String to be converted
+   * @return DecodeResult Success storing T type
+   */
   def stringTo[T](fromTo: String => T): String => DecodeResult[T] =
     fromTo.andThen(DecodeResult.Success(_))
 }
