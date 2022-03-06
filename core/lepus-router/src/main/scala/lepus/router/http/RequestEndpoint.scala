69f28d82fd39acdf6cd7aff643ae3afbd4fd51e2
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-03-06 20:05:14 +0900

    Add comment

--- core/lepus-router/src/main/scala/lepus/router/http/RequestEndpoint.scala
+++ core/lepus-router/src/main/scala/lepus/router/http/RequestEndpoint.scala
@@ -21,14 +21,54 @@ sealed trait RequestEndpoint[T] {
 
 object RequestEndpoint {
 
+  /**
+   * fixed-character path
+   *
+   * @param name      Path Name
+   * @param converter For converting String paths to any T type
+   * @tparam T        Parameters of the type you want to convert String to
+   */
   case class FixedPath[T](name: String, converter: EndpointConverter[String, T]) extends RequestEndpoint[T]
+
+  /**
+   * Dynamically changing path parameters
+   *
+   * @param name      Name of path parameter, used for Swagger (Open Api) documentation generation, etc.
+   * @param converter For converting String paths to any T type
+   * @tparam T        Parameters of the type you want to convert String to
+   */
   case class PathParam[T](name: String, converter: EndpointConverter[String, T]) extends RequestEndpoint[T] {
     def validate(validator: Validator): ValidateParam[T] = ValidateParam(name, converter, validator)
   }
+
+  /**
+   * Query parameter
+   *
+   * @param key       The key name of the query parameter, which is also used to generate Swagger (Open Api) documentation
+   * @param converter For converting String paths to any T type
+   * @tparam T        Parameters of the type you want to convert String to
+   */
   case class QueryParam[T](key: String, converter: EndpointConverter[String, T]) extends RequestEndpoint[T]
 
+  /**
+   * Validation defined value for dynamically changing path parameters
+   *
+   * @param name      Name of path parameter, used for Swagger (Open Api) documentation generation, etc.
+   * @param converter For converting String paths to any T type
+   * @param validator Validation settings to validate path parameters
+   * @tparam T        Parameters of the type you want to convert String to
+   */
   case class ValidateParam[T](name: String, converter: EndpointConverter[String, T], validator: Validator) extends RequestEndpoint[T]
 
+  /**
+   * Model to store RequestEndpoint pairs
+   *
+   * @param left  Pairs of RequestEndpoints are stored from left to right in the HTTP URL path.
+   * @param right The RequestEndpoint currently referenced in the HTTP URL path is stored.
+   * @tparam L    Type parameter to adapt to the path stored in the RequestEndpoint pair
+   * @tparam R    Type parameter applied to the path of the currently referenced RequestEndpoint
+   * @tparam LR   L, R compound
+   */
   case class Pair[L, R, LR](
     left:  RequestEndpoint[L],
     right: RequestEndpoint[R]
