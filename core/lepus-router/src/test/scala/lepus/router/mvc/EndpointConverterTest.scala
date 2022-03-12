9e4460f061457b747211fc0cedeaaf3767672853
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-03-12 19:26:18 +0900

    Add Scala collection EndpointConverter Test

--- core/lepus-router/src/test/scala/lepus/router/mvc/EndpointConverterTest.scala
+++ core/lepus-router/src/test/scala/lepus/router/mvc/EndpointConverterTest.scala
@@ -7,11 +7,13 @@
 package lepus.router.mvc
 
 import java.time._
-import java.util._
+import java.util.UUID
 
 import scala.reflect.ClassTag
 
-import org.scalacheck.Arbitrary
+import org.scalacheck.{ Arbitrary, Gen }
+import org.scalacheck.Prop._
+
 import org.scalatest.Assertion
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -27,6 +29,9 @@ class EndpointConverterTest extends AnyFlatSpec with Matchers with Checkers {
     checkDecodeFromString[Short]
     checkDecodeFromString[Int]
     checkDecodeFromString[Long]
+    checkDecodeFromString[Float]
+    checkDecodeFromString[Double]
+    checkDecodeFromString[Boolean]
     checkDecodeFromString[BigDecimal]
     checkDecodeFromString[LocalTime]
     checkDecodeFromString[LocalDate]
@@ -36,6 +41,44 @@ class EndpointConverterTest extends AnyFlatSpec with Matchers with Checkers {
     checkDecodeFromString[ZonedDateTime]
     checkDecodeFromString[Instant]
     checkDecodeFromString[UUID]
+  }
+
+  it should "assert array types using .mkString" in {
+    checkDecodeFromArray[List, String]()
+    checkDecodeFromArray[List, Byte]()
+    checkDecodeFromArray[List, Short]()
+    checkDecodeFromArray[List, Int]()
+    checkDecodeFromArray[List, Long]()
+    checkDecodeFromArray[List, Float]()
+    checkDecodeFromArray[List, Double]()
+    checkDecodeFromArray[List, Boolean]()
+    checkDecodeFromArray[List, BigDecimal]()
+    checkDecodeFromArray[List, LocalTime]()
+    checkDecodeFromArray[List, LocalDate]()
+    checkDecodeFromArray[List, LocalDateTime]()
+    checkDecodeFromArray[List, OffsetTime]()
+    checkDecodeFromArray[List, OffsetDateTime]()
+    checkDecodeFromArray[List, ZonedDateTime]()
+    checkDecodeFromArray[List, Instant]()
+    checkDecodeFromArray[List, UUID]()
+
+    checkDecodeFromArray[Seq, String]()
+    checkDecodeFromArray[Seq, Byte]()
+    checkDecodeFromArray[Seq, Short]()
+    checkDecodeFromArray[Seq, Int]()
+    checkDecodeFromArray[Seq, Long]()
+    checkDecodeFromArray[Seq, Float]()
+    checkDecodeFromArray[Seq, Double]()
+    checkDecodeFromArray[Seq, Boolean]()
+    checkDecodeFromArray[Seq, BigDecimal]()
+    checkDecodeFromArray[Seq, LocalTime]()
+    checkDecodeFromArray[Seq, LocalDate]()
+    checkDecodeFromArray[Seq, LocalDateTime]()
+    checkDecodeFromArray[Seq, OffsetTime]()
+    checkDecodeFromArray[Seq, OffsetDateTime]()
+    checkDecodeFromArray[Seq, ZonedDateTime]()
+    checkDecodeFromArray[Seq, Instant]()
+    checkDecodeFromArray[Seq, UUID]()
   }
 
   def checkDecodeFromString[T: Arbitrary](implicit converter: EndpointConverter[String, T], classTag: ClassTag[T]): Assertion =
@@ -48,4 +91,21 @@ class EndpointConverterTest extends AnyFlatSpec with Matchers with Checkers {
         decoded === v
       })
     }
+
+  def checkDecodeFromArray[F[_], T: Arbitrary]()(implicit converter: EndpointConverter[String, F[T]], classTag: ClassTag[F[T]]): Assertion = {
+    val gen  = Gen.containerOf[Iterable, T](Arbitrary.arbitrary[T])
+    val list = gen.sample.getOrElse(Iterable.empty[T])
+    converter.decode(list.iterator.mkString(",")) match {
+      case Success(value)         =>
+        val decoded = value.asInstanceOf[IterableOnce[T]]
+        assert(
+          classTag.runtimeClass.isInstance(decoded) &&
+          classTag.runtimeClass.isInstance(list)    &&
+          decoded === list                          &&
+          decoded.iterator.mkString(",") === list.iterator.mkString(",")
+        )
+      case InvalidValue(value, _) => assert(value === list.iterator.mkString(","))
+      case unexpected             => fail(s"Value ${list.iterator.mkString(",")} got decoded to unexpected $unexpected")
+    }
+  }
 }
