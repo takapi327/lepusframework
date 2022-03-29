26c85becee1f6aa7b65392f11091b139388ad347
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-03-29 20:44:15 +0900

    Create Request

---
+++ core/lepus-router/src/main/scala/lepus/router/http/Request.scala
@@ -0,0 +1,38 @@
+/**
+ *  This file is part of the Lepus Framework.
+ *  For the full copyright and license information,
+ *  please view the LICENSE file that was distributed with this source code.
+ */
+
+package lepus.router.http
+
+import org.http4s.{ Request => Http4sRequest, Uri }
+
+trait HttpRequest {
+  def method: RequestMethod
+  def pathSegments: List[String]
+  def queryParameters: Map[String, Seq[String]]
+}
+
+class Request[F[_]](request: Http4sRequest[F]) extends HttpRequest {
+  lazy val method: RequestMethod = request.method.name.toUpperCase match {
+    case "GET"     => RequestMethod.Get
+    case "HEAD"    => RequestMethod.Head
+    case "POST"    => RequestMethod.Post
+    case "PUT"     => RequestMethod.Put
+    case "DELETE"  => RequestMethod.Delete
+    case "OPTIONS" => RequestMethod.Options
+    case "PATCH"   => RequestMethod.Patch
+    case "CONNECT" => RequestMethod.Connect
+    case "TRACE"   => RequestMethod.Trace
+    case _         => throw new NoSuchElementException("The request method received did not match the expected value.")
+  }
+
+  lazy val pathSegments: List[String] = {
+    request.pathInfo.renderString
+      .dropWhile(_ == '/').split("/")
+      .toList.map(Uri.decode(_))
+  }
+
+  lazy val queryParameters: Map[String, Seq[String]] = request.multiParams
+}
