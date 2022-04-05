652bcd98bde0b56b1c7debd8bb4cbece366be3e2
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-04-05 23:24:36 +0900

    Creation of response body with 400 series status

--- core/lepus-router/src/main/scala/lepus/router/model/ServerResponse.scala
+++ core/lepus-router/src/main/scala/lepus/router/model/ServerResponse.scala
@@ -68,4 +68,69 @@ object ServerResponse {
   /** Generates a ‘207 MULTI_STATUS’ result. */
   val MultiStatus = new Result(ResponseStatus.MultiStatus)
 
+  /** Generates a ‘400 BAD_REQUEST’ result. */
+  val BadRequest = new Result(ResponseStatus.BadRequest)
+
+  /** Generates a ‘401 UNAUTHORIZED’ result. */
+  val Unauthorized = new Result(ResponseStatus.Unauthorized)
+
+  /** Generates a ‘402 PAYMENT_REQUIRED’ result. */
+  val PaymentRequired = new Result(ResponseStatus.PaymentRequired)
+
+  /** Generates a ‘403 FORBIDDEN’ result. */
+  val Forbidden = new Result(ResponseStatus.Forbidden)
+
+  /** Generates a ‘404 NOT_FOUND’ result. */
+  val NotFound = new Result(ResponseStatus.NotFound)
+
+  /** Generates a ‘405 METHOD_NOT_ALLOWED’ result. */
+  val MethodNotAllowed = new Result(ResponseStatus.MethodNotAllowed)
+
+  /** Generates a ‘406 NOT_ACCEPTABLE’ result. */
+  val NotAcceptable = new Result(ResponseStatus.NotAcceptable)
+
+  /** Generates a ‘408 REQUEST_TIMEOUT’ result. */
+  val RequestTimeout = new Result(ResponseStatus.RequestTimeout)
+
+  /** Generates a ‘409 CONFLICT’ result. */
+  val Conflict = new Result(ResponseStatus.Conflict)
+
+  /** Generates a ‘410 GONE’ result. */
+  val Gone = new Result(ResponseStatus.Gone)
+
+  /** Generates a ‘412 PRECONDITION_FAILED’ result. */
+  val PreconditionFailed = new Result(ResponseStatus.PreconditionFailed)
+
+  /** Generates a ‘413 REQUEST_ENTITY_TOO_LARGE’ result. */
+  val EntityTooLarge = new Result(ResponseStatus.RequestEntityTooLarge)
+
+  /** Generates a ‘414 REQUEST_URI_TOO_LONG’ result. */
+  val UriTooLong = new Result(ResponseStatus.RequestUriTooLong)
+
+  /** Generates a ‘415 UNSUPPORTED_MEDIA_TYPE’ result. */
+  val UnsupportedMediaType = new Result(ResponseStatus.UnsupportedMediaType)
+
+  /** Generates a ‘417 EXPECTATION_FAILED’ result. */
+  val ExpectationFailed = new Result(ResponseStatus.ExpectationFailed)
+
+  /** Generates a ‘418 IM_A_TEAPOT’ result. */
+  val ImATeapot = new Result(ResponseStatus.ImATeapot)
+
+  /** Generates a ‘422 UNPROCESSABLE_ENTITY’ result. */
+  val UnprocessableEntity = new Result(ResponseStatus.UnprocessableEntity)
+
+  /** Generates a ‘423 LOCKED’ result. */
+  val Locked = new Result(ResponseStatus.Locked)
+
+  /** Generates a ‘424 FAILED_DEPENDENCY’ result. */
+  val FailedDependency = new Result(ResponseStatus.FailedDependency)
+
+  /** Generates a ‘428 PRECONDITION_REQUIRED’ result. */
+  val PreconditionRequired = new Result(ResponseStatus.PreconditionRequired)
+
+  /** Generates a ‘429 TOO_MANY_REQUESTS’ result. */
+  val TooManyRequests = new Result(ResponseStatus.TooManyRequests)
+
+  /** Generates a ‘431 REQUEST_HEADER_FIELDS_TOO_LARGE’ result. */
+  val RequestHeaderFieldsTooLarge = new Result(ResponseStatus.RequestHeaderFieldsTooLarge)
 }
