d46c195c5d5624c7eb3ebee90ea0049d1073095e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-04-17 01:28:24 +0900

    Cut out the Parameter defined in Path to a separate file

--- development/lepus-swagger/src/main/scala/lepus/swagger/model/Path.scala
+++ development/lepus-swagger/src/main/scala/lepus/swagger/model/Path.scala
@@ -9,7 +9,6 @@ package lepus.swagger.model
 import io.circe._
 import io.circe.generic.semiauto._
 
-import lepus.router.model.Schema
 import lepus.router.http.RequestEndpoint
 import lepus.router.RouterConstructor
 
@@ -52,52 +51,6 @@ object Path {
       deprecated  = router.deprecated,
       parameters  = parameters
     )
-  }
-}
-
-/**
- * Model representing parameters given to Http requests.
- *
- * @param name        parameter identifier
- * @param in          Classification of this parameter
- * @param required    Value of whether this parameter is required at Http request time.
- * @param schema      This parameter type
- * @param description Description of this parameter
- */
-final case class Parameter(
-  name:        String,
-  in:          String,
-  required:    Boolean,
-  schema:      Schema,
-  description: Option[String],
-)
-
-object Parameter {
-  implicit lazy val encoder: Encoder[Parameter] = deriveEncoder
-
-  def fromRequestEndpoint(endpoint: RequestEndpoint.Path with RequestEndpoint.Param): Parameter =
-    Parameter(
-      name        = endpoint.name,
-      in          = ParameterInType.PATH,
-      required    = true,
-      schema      = endpoint.converter.schema,
-      description = endpoint.description,
-    )
-
-  def fromRequestEndpoint(endpoint: RequestEndpoint.Query with RequestEndpoint.Param): Parameter =
-    Parameter(
-      name        = endpoint.key,
-      in          = ParameterInType.QUERY,
-      required    = false,
-      schema      = endpoint.converter.schema,
-      description = endpoint.description,
-    )
-
-  object ParameterInType {
-    val PATH   = "path"
-    val QUERY  = "query"
-    val HEADER = "header"
-    val COOKIE = "cookie"
   }
 }
 
