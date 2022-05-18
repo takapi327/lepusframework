be32bd971865e2fd12255c5dd7fae8678034218e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-05-18 22:21:46 +0900

    Add implicit schema valiable

--- core/lepus-router/src/main/scala/lepus/router/model/Schema.scala
+++ core/lepus-router/src/main/scala/lepus/router/model/Schema.scala
@@ -4,6 +4,10 @@
 
 package lepus.router.model
 
+import java.time._
+import java.util.{ Date, UUID }
+import java.math.{ BigDecimal => JBigDecimal, BigInteger => JBigInteger }
+
 import io.circe._
 import io.circe.generic.semiauto._
 
@@ -47,4 +51,79 @@ object Schema {
     val PASSWORD: Option[String] = Some("password")
     val UUID:     Option[String] = Some("uuid")
   }
+}
+
+import SchemaType._
+final case class SchemaL[T](
+  schemaType: SchemaType[T],
+  name:       Option[SchemaL.Name] = None,
+  format:     Option[String]       = None,
+  isOptional: Boolean              = false
+) {
+
+  def thisType: String = s"schema is $schemaType"
+
+  def format(f: String): SchemaL[T] = copy(format = Some(f))
+
+  /** Returns an optional version of this schema, with `isOptional` set to true. */
+  def asOption: SchemaL[Option[T]] =
+    SchemaL(
+      schemaType = SOption(this),
+      isOptional = true,
+      format     = format
+    )
+
+  /** Returns an array version of this schema, with the schema type wrapped in [[SArray]]. Sets `isOptional` to true as
+    * the collection might be empty.
+    */
+  def asArray: SchemaL[Array[T]] =
+    SchemaL(
+      schemaType = SArray(this),
+      isOptional = true
+    )
+
+  /** Returns a collection version of this schema, with the schema type wrapped in [[SArray]]. Sets `isOptional` to true
+    * as the collection might be empty.
+    */
+  def asIterable[C[X] <: Iterable[X]]: SchemaL[C[T]] =
+    SchemaL(
+      schemaType = SArray(this),
+      isOptional = true
+    )
+}
+
+object SchemaL {
+
+  case class Name(fullName: String, typeParameters: List[String])
+
+  implicit val schemaString:         SchemaL[String]         = SchemaL(SString())
+  implicit val schemaByte:           SchemaL[Byte]           = SchemaL(SInteger())
+  implicit val schemaShort:          SchemaL[Short]          = SchemaL(SInteger())
+  implicit val schemaInt:            SchemaL[Int]            = SchemaL(SInteger[Int]()).format("int32")
+  implicit val schemaLong:           SchemaL[Long]           = SchemaL(SInteger[Long]()).format("int64")
+  implicit val schemaFloat:          SchemaL[Float]          = SchemaL(SNumber[Float]()).format("float")
+  implicit val schemaDouble:         SchemaL[Double]         = SchemaL(SNumber[Double]()).format("double")
+  implicit val schemaBoolean:        SchemaL[Boolean]        = SchemaL(SBoolean())
+  implicit val schemaUnit:           SchemaL[Unit]           = SchemaL(SUnit())
+  implicit val schemaByteArray:      SchemaL[Array[Byte]]    = SchemaL(SBinary())
+  implicit val schemaInstant:        SchemaL[Instant]        = SchemaL(SBinary())
+  implicit val schemaZonedDateTime:  SchemaL[ZonedDateTime]  = SchemaL(SDateTime())
+  implicit val schemaOffsetDateTime: SchemaL[OffsetDateTime] = SchemaL(SDateTime())
+  implicit val schemaDate:           SchemaL[Date]           = SchemaL(SDateTime())
+  implicit val schemaLocalDateTime:  SchemaL[LocalDateTime]  = SchemaL(SString())
+  implicit val schemaLocalDate:      SchemaL[LocalDate]      = SchemaL(SDate())
+  implicit val schemaZoneOffset:     SchemaL[ZoneOffset]     = SchemaL(SString())
+  implicit val schemaJavaDuration:   SchemaL[Duration]       = SchemaL(SString())
+  implicit val schemaLocalTime:      SchemaL[LocalTime]      = SchemaL(SString())
+  implicit val schemaOffsetTime:     SchemaL[OffsetTime]     = SchemaL(SString())
+  implicit val schemaScalaDuration: SchemaL[scala.concurrent.duration.Duration] = SchemaL(SString())
+  implicit val schemaUUID:        SchemaL[UUID]        = SchemaL(SString[UUID]()).format("uuid")
+  implicit val schemaBigDecimal:  SchemaL[BigDecimal]  = SchemaL(SNumber())
+  implicit val schemaJBigDecimal: SchemaL[JBigDecimal] = SchemaL(SNumber())
+  implicit val schemaBigInt:      SchemaL[BigInt]      = SchemaL(SInteger())
+  implicit val schemaJBigInteger: SchemaL[JBigInteger] = SchemaL(SInteger())
+
+  implicit def schemaOption[T: SchemaL]: SchemaL[Option[T]] = implicitly[SchemaL[T]].asOption
+  implicit def schemaArray[T: SchemaL]:  SchemaL[Array[T]]  = implicitly[SchemaL[T]].asArray
+  implicit def schemaIterable[T: SchemaL, C[X] <: Iterable[X]]: SchemaL[C[T]] = implicitly[SchemaL[T]].asIterable[C]
 }
