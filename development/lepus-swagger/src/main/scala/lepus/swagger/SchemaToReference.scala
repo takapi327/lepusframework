f1eea7daccabf6e1da72b04cd816edc7a2a6d35b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-05-22 18:08:43 +0900

    Create SchemaToReference

---
+++ development/lepus-swagger/src/main/scala/lepus/swagger/SchemaToReference.scala
@@ -0,0 +1,19 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus.swagger
+
+import scala.collection.immutable.ListMap
+
+import lepus.router.model.Schema
+
+import lepus.swagger.model.Reference
+
+class SchemaToReference(nameMapList: Option[ListMap[Schema.Name, Schema[_]]]) {
+  def map(name: Schema.Name): Option[Reference] =
+    nameMapList.map(_.get(name)) match {
+      case Some(_) => Some(Reference(s"#/components/schemas/${name.shortName}"))
+      case None    => None
+    }
+}
