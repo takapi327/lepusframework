aaebb85bfbd9d369272f956850414b26aa8ad720
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-05-29 21:07:08 +0900

    Add semiauto example comment

--- core/lepus-router/src/main/scala2.13/lepus/router/generic/semiauto.scala
+++ core/lepus-router/src/main/scala2.13/lepus/router/generic/semiauto.scala
@@ -1,6 +1,6 @@
 /** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  * file that was distributed with this source code.
+  */
 
 package lepus.router.generic
 
@@ -10,6 +10,23 @@ import magnolia1._
 
 import lepus.router.model.Schema
 
+/** Semi-automatic Schema derivation.
+  *
+  * This object provides helpers for creating [[lepus.router.model.Schema]] instances for case classes
+  *
+  * Typical usage will look like the following:
+  *
+  * {{{
+  *   import lepus.router.model.Schema
+  *   import lepus.router.generic.semiauto._
+  *
+  *   case class Foo(i: Int, p: (String, Double))
+  *
+  *   object Foo {
+  *     implicit val schema: Schema[Foo] = deriveSchemer[Foo]
+  *   }
+  * }}}
+  */
 object semiauto extends SchemaDerivation {
   implicit def deriveSchemer[T]: Schema[T] = macro Magnolia.gen[T]
 }
--- core/lepus-router/src/main/scala3/lepus/router/generic/semiauto.scala
+++ core/lepus-router/src/main/scala3/lepus/router/generic/semiauto.scala
@@ -11,5 +11,23 @@ import magnolia1.*
 import lepus.router.*
 import model.Schema
 
+/**
+ * Semi-automatic Schema derivation.
+ *
+ * This object provides helpers for creating [[lepus.router.model.Schema]]  instances for case classes
+ *
+ * Typical usage will look like the following:
+ *
+ * {{{
+ *   import lepus.router.model.Schema
+ *   import lepus.router.generic.semiauto.*
+ *
+ *   case class Foo(i: Int, p: (String, Double))
+ *
+ *   object Foo {
+ *     given schema: Schema[Foo] = deriveSchemer[Foo]
+ *   }
+ * }}}
+ */
 object semiauto extends SchemaDerivation:
   inline final def deriveSchemer[T](using inline M: Mirror.Of[T]): Schema[T] = derived[T]
