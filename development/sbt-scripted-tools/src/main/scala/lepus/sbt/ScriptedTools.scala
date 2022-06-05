52b8e4ca95c382a9f02573347a66de5400a764ce
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-06-05 21:27:39 +0900

    Fix Scala format

--- development/sbt-scripted-tools/src/main/scala/lepus/sbt/ScriptedTools.scala
+++ development/sbt-scripted-tools/src/main/scala/lepus/sbt/ScriptedTools.scala
@@ -1,8 +1,6 @@
-/**
- *  This file is part of the Lepus Framework.
- *  For the full copyright and license information,
- *  please view the LICENSE file that was distributed with this source code.
- */
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+  * file that was distributed with this source code.
+  */
 
 package lepus.sbt
 
@@ -10,39 +8,36 @@ import java.net.URL
 
 import sbt._
 
-/**
- * A collection of tools to validate the Sbt plugin.
- */
+/** A collection of tools to validate the Sbt plugin.
+  */
 object ScriptedTools extends AutoPlugin {
 
   override def trigger = allRequirements
 
   private val TIMEOUT_NUMBER = 10000
 
-  /**
-   * Methods for calling the api of the specified path.
-   *
-   * @param path
-   * The path of the API to call.
-   * @return
-   * The response status and InputStream value resulting from the API call.
-   */
+  /** Methods for calling the api of the specified path.
+    *
+    * @param path
+    *   The path of the API to call.
+    * @return
+    *   The response status and InputStream value resulting from the API call.
+    */
   def callUrl(path: String): (Int, String) = {
     callUrl("localhost", 5555, path)
   }
 
-  /**
-   * Method to invoke api for the specified host and port and path.
-   *
-   * @param host
-   * The host of the API to call.
-   * @param port
-   * The port of the API to call.
-   * @param path
-   * The path of the API to call.
-   * @return
-   * The response status and InputStream value resulting from the API call.
-   */
+  /** Method to invoke api for the specified host and port and path.
+    *
+    * @param host
+    *   The host of the API to call.
+    * @param port
+    *   The port of the API to call.
+    * @param path
+    *   The path of the API to call.
+    * @return
+    *   The response status and InputStream value resulting from the API call.
+    */
   def callUrl(
     host: String,
     port: Int,
@@ -51,14 +46,13 @@ object ScriptedTools extends AutoPlugin {
     callUrlImpl(url(s"http://$host:$port/$path"))
   }
 
-  /**
-   * Method to call the api of the specified URL.
-   *
-   * @param url
-   * The url of the API to call.
-   * @return
-   * The response status and InputStream value resulting from the API call.
-   */
+  /** Method to call the api of the specified URL.
+    *
+    * @param url
+    *   The url of the API to call.
+    * @return
+    *   The response status and InputStream value resulting from the API call.
+    */
   def callUrlImpl(url: URL): (Int, String) = {
     val connection = url.openConnection().asInstanceOf[java.net.HttpURLConnection]
     connection.setConnectTimeout(TIMEOUT_NUMBER)
