afbbb27780b04b631a6e6600fd65b093aa088b6c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-06-09 23:40:06 +0900

    Fix LogbackJsonEncoder

--- core/lepus-logback/src/main/scala/lepus/logback/LogbackTrackingEncoder.scala
+++ core/lepus-logback/src/main/scala/lepus/logback/LogbackTrackingEncoder.scala
@@ -1,6 +1,6 @@
 /** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  * file that was distributed with this source code.
+  */
 
 package lepus.logback
 
@@ -10,23 +10,23 @@ import ch.qos.logback.core.CoreConstants
 import ch.qos.logback.classic.spi.ILoggingEvent
 
 /** Outputs system and application generated logs in a traceable format. Configure logback.groovy, logback-test.xml, or
- * logback.xml on the application classpath as follows
- *
- * For example:
- * {{{
- *   <configuration>
- *     <appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
- *       <encoder class="ch.qos.logback.core.encoder.LayoutWrappingEncoder">
- *         <layout class="lepus.logback.LogbackTrackingEncoder" />
- *       </encoder>
- *     </appender>
- *
- *     <root level="INFO">
- *       <appender-ref ref="STDOUT" />
- *     </root>
- *   </<configuration>
- * }}}
- */
+  * logback.xml on the application classpath as follows
+  *
+  * For example:
+  * {{{
+  *   <configuration>
+  *     <appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
+  *       <encoder class="ch.qos.logback.core.encoder.LayoutWrappingEncoder">
+  *         <layout class="lepus.logback.LogbackTrackingEncoder" />
+  *       </encoder>
+  *     </appender>
+  *
+  *     <root level="INFO">
+  *       <appender-ref ref="STDOUT" />
+  *     </root>
+  *   </<configuration>
+  * }}}
+  */
 class LogbackTrackingEncoder extends BaseEncoder {
   override def doLayout(event: ILoggingEvent): String = {
     val sbuf: StringBuffer = new StringBuffer(128)
