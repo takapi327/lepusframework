454f632d28499a6450eeb634e8ecc8d91816dad1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-06-09 23:15:40 +0900

    Create BaseEncoder

---
+++ core/lepus-logback/src/main/scala/lepus/logback/BaseEncoder.scala
@@ -0,0 +1,29 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus.logback
+
+
+import java.time.{ LocalDateTime, Instant }
+import java.util.TimeZone
+
+import ch.qos.logback.core.LayoutBase
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.classic._
+
+trait BaseEncoder extends LayoutBase[ILoggingEvent] with ColorLog {
+
+  protected def colorLevel(event: ILoggingEvent): String = {
+    event.getLevel match {
+      case Level.TRACE => white("TRACE")
+      case Level.DEBUG => cyan("DEBUG")
+      case Level.INFO  => blue("INFO")
+      case Level.WARN  => yellow("WARN")
+      case Level.ERROR => red("ERROR")
+    }
+  }
+
+  protected def timeStampToLocalDateTime(timeStamp: Long): LocalDateTime =
+    LocalDateTime.ofInstant(Instant.ofEpochMilli(timeStamp), TimeZone.getDefault.toZoneId)
+}
