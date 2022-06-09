2812b2884191fb5568017bd15eea4201204daf8d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-06-09 23:40:10 +0900

    Fix Scala format

--- core/lepus-logback/src/main/scala/lepus/logback/BaseEncoder.scala
+++ core/lepus-logback/src/main/scala/lepus/logback/BaseEncoder.scala
@@ -1,9 +1,8 @@
 /** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  * file that was distributed with this source code.
+  */
 
 package lepus.logback
-
 
 import java.time.{ LocalDateTime, Instant }
 import java.util.TimeZone
