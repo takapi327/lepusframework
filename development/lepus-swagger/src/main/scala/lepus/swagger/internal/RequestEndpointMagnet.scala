cac59fa90bb132dc94614f149bcf02a28895805c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-07-24 00:11:35 +0900

    Convert RequestEndpointMagnet to Scala3 code

--- development/lepus-swagger/src/main/scala/lepus/swagger/internal/RequestEndpointMagnet.scala
+++ development/lepus-swagger/src/main/scala/lepus/swagger/internal/RequestEndpointMagnet.scala
@@ -4,44 +4,46 @@
 
 package lepus.swagger.internal
 
-import lepus.router.http.RequestEndpoint
+import lepus.router.http.RequestEndpoint.*
 
 import lepus.swagger.model.Parameter
 import lepus.swagger.SchemaToOpenApiSchema
+
 import Parameter.ParameterInType
 
-trait RequestEndpointMagnet {
+trait RequestEndpointMagnet:
   type ThisType
   def toParameter(schemaToOpenApiSchema: SchemaToOpenApiSchema): ThisType
-}
 
-object RequestEndpointMagnet {
+object RequestEndpointMagnet:
 
-  implicit def pathMagnet(
-    endpoint: RequestEndpoint.Path with RequestEndpoint.Param
-  ): RequestEndpointMagnet = new RequestEndpointMagnet {
-    override type ThisType = Parameter
+  type PathParam  = Path & Param
+  type QueryParam = Query & Param
 
-    override def toParameter(schemaToOpenApiSchema: SchemaToOpenApiSchema): ThisType = Parameter(
-      name        = endpoint.name,
-      in          = ParameterInType.PATH,
-      required    = true,
-      schema      = schemaToOpenApiSchema(endpoint.converter.schema, false, false),
-      description = endpoint.description
-    )
-  }
+  given Conversion[PathParam, RequestEndpointMagnet] with
+    override def apply(endpoint: PathParam): RequestEndpointMagnet =
+      new RequestEndpointMagnet {
+        override type ThisType = Parameter
 
-  implicit def queryMagnet(
-    endpoint: RequestEndpoint.Query with RequestEndpoint.Param
-  ): RequestEndpointMagnet = new RequestEndpointMagnet {
-    override type ThisType = Parameter
+        override def toParameter(schemaToOpenApiSchema: SchemaToOpenApiSchema): ThisType = Parameter(
+          name        = endpoint.name,
+          in          = ParameterInType.PATH,
+          required    = true,
+          schema      = schemaToOpenApiSchema(endpoint.converter.schema, false, false),
+          description = endpoint.description
+        )
+      }
 
-    override def toParameter(schemaToOpenApiSchema: SchemaToOpenApiSchema): ThisType = Parameter(
-      name        = endpoint.key,
-      in          = ParameterInType.QUERY,
-      required    = false,
-      schema      = schemaToOpenApiSchema(endpoint.converter.schema, false, false),
-      description = endpoint.description
-    )
-  }
-}
+  given Conversion[QueryParam, RequestEndpointMagnet] with
+    override def apply(endpoint: QueryParam): RequestEndpointMagnet =
+      new RequestEndpointMagnet {
+        override type ThisType = Parameter
+
+        override def toParameter(schemaToOpenApiSchema: SchemaToOpenApiSchema): ThisType = Parameter(
+          name        = endpoint.key,
+          in          = ParameterInType.QUERY,
+          required    = false,
+          schema      = schemaToOpenApiSchema(endpoint.converter.schema, false, false),
+          description = endpoint.description
+        )
+      }
