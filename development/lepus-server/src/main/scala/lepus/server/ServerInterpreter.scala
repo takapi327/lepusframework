5e3834689d8f777d25133a27a6995a861bc623a0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-07-24 23:27:13 +0900

    Replace ServerInterpreter lepus-router -> lepus-server/s

--- development/lepus-server/src/main/scala/lepus/server/ServerInterpreter.scala
+++ development/lepus-server/src/main/scala/lepus/server/ServerInterpreter.scala
@@ -2,37 +2,39 @@
   * file that was distributed with this source code.
   */
 
-package lepus.router
+package lepus.server
 
 import cats.data.{ Kleisli, OptionT }
-import cats.implicits._
+import cats.implicits.*
 import cats.effect.{ Async, Sync }
 
 import org.http4s.{ Request => Http4sRequest, HttpRoutes => Http4sRoutes, Response => Http4sResponse }
 
-import lepus.router.http._
-import lepus.router.internal._
+import lepus.router.*
+import lepus.router.http.*
+import lepus.router.internal.*
 import lepus.router.model.{ ServerRequest, ServerResponse }
-import ConvertResult._
+import lepus.router.ConvertResult.*
+
 
 /** Compare and verify Http requests and endpoints, and combine them with logic.
-  *
-  * @tparam F
-  *   the effect type.
-  */
-trait ServerInterpreter[F[_]](using syncF: Sync[F], asyncF: Async[F]):
+ *
+ * @tparam F
+ *   the effect type.
+ */
+trait ServerInterpreter[F[_]](using Sync[F], Async[F]):
 
   /** Receives HTTP requests, compares and verifies them with endpoints, and binds them to server logic.
-    *
-    * @param routes
-    *   Server logic for each HTTP method
-    * @param endpoint
-    *   Endpoints you expect to receive as requests
-    * @tparam T
-    *   Type of parameters to be received in the request
-    * @return
-    *   If the request and endpoint match, http4s HttpRoutes are returned and the server logic is executed.
-    */
+   *
+   * @param routes
+   *   Server logic for each HTTP method
+   * @param endpoint
+   *   Endpoints you expect to receive as requests
+   * @tparam T
+   *   Type of parameters to be received in the request
+   * @return
+   *   If the request and endpoint match, http4s HttpRoutes are returned and the server logic is executed.
+   */
   def bindFromRequest[T](routes: HttpRoutes[F, T], endpoint: RequestEndpoint.Endpoint): Http4sRoutes[F] =
     Kleisli[[T] =>> OptionT[F, T], Http4sRequest[F], Http4sResponse[F]] { (http4sRequest: Http4sRequest[F]) =>
       val request = Request[F](http4sRequest)
@@ -40,17 +42,17 @@ trait ServerInterpreter[F[_]](using syncF: Sync[F], asyncF: Async[F]):
       for
         logic        <- OptionT.fromOption[F] { routes.lift(request.method) }
         decodeResult <- OptionT.fromOption[F] { decodeRequest[T](request, endpoint) }
-        response     <- OptionT { logic(using ServerRequest[F, T](http4sRequest, decodeResult)).map(_.some) }
+        response     <- OptionT { logic(ServerRequest[F, T](http4sRequest, decodeResult)).map(_.some) }
       yield addResponseHeader(response).toHttp4sResponse()
     }
 
   /** Add response headers according to body
-    *
-    * @param response
-    *   Logic return value corresponding to the endpoint
-    * @return
-    *   ServerResponse with headers according to the contents of the body
-    */
+   *
+   * @param response
+   *   Logic return value corresponding to the endpoint
+   * @return
+   *   ServerResponse with headers according to the contents of the body
+   */
   def addResponseHeader(response: ServerResponse): ServerResponse =
     response.body match
       case None => response
@@ -61,16 +63,16 @@ trait ServerInterpreter[F[_]](using syncF: Sync[F], asyncF: Async[F]):
           case _            => response
 
   /** Verify that the actual request matches the endpoint that was intended to be received as a request.
-    *
-    * @param request
-    *   HTTP request for http4s to pass to Server
-    * @param endpoint
-    *   Endpoints you expect to receive as requests
-    * @tparam T
-    *   Type of parameters to be received in the request
-    * @return
-    *   If the request and endpoint match, return Some; if not, return None.
-    */
+   *
+   * @param request
+   *   HTTP request for http4s to pass to Server
+   * @param endpoint
+   *   Endpoints you expect to receive as requests
+   * @tparam T
+   *   Type of parameters to be received in the request
+   * @return
+   *   If the request and endpoint match, return Some; if not, return None.
+   */
   private def decodeRequest[T](
     request:  HttpRequest,
     endpoint: RequestEndpoint.Endpoint
@@ -81,7 +83,8 @@ trait ServerInterpreter[F[_]](using syncF: Sync[F], asyncF: Async[F]):
       case DecodeEndpointResult.Success(values) =>
         Some(
           (if values.nonEmpty then values.toTuple
-           else {
-             // TODO: If there is no value to pass to the logic, Unit is returned, but Nothing can be returned.
-           }).asInstanceOf[T]
+          else {
+            // TODO: If there is no value to pass to the logic, Unit is returned, but Nothing can be returned.
+          }).asInstanceOf[T]
         )
+
