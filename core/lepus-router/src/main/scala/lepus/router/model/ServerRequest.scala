8602e785668b59a31dd5c4f1bc802110516a9cca
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-07-27 19:02:43 +0900

    Fix ServerRequest

--- core/lepus-router/src/main/scala/lepus/router/model/ServerRequest.scala
+++ core/lepus-router/src/main/scala/lepus/router/model/ServerRequest.scala
@@ -4,19 +4,35 @@
 
 package lepus.router.model
 
-import org.http4s.Request
+import scala.annotation.targetName
+
+import cats.MonadThrow
+
+import org.http4s.{ Request, EntityDecoder }
 
 import lepus.router.http.Header
 
 class ServerRequest[F[_], T](request: Request[F], val param: T):
 
-  lazy val protocol: String = request.httpVersion.toString()
+  opaque type Protocol = String
+  extension (prot: Protocol)
+    @targetName("protocolAsString") def asString: String = prot
+
+  opaque type ContentType = String
+  extension (content: ContentType)
+    @targetName("contentTypeAsString") def asString: String = content
+
+  opaque type ContentLength = Long
+  extension (content: ContentLength)
+    def asLong: Long = content
+
+  lazy val protocol: Protocol = request.httpVersion.toString()
 
   lazy val headers: Seq[Header] =
     request.headers.headers.map(header => Header(header.name.toString, header.value))
 
-  lazy val contentType:   Option[String] = findHeaderValue(Header.CONTENT_TYPE)
-  lazy val contentLength: Option[Long]   = findHeaderValue(Header.CONTENT_LENGTH).flatMap(_.toLongOption)
+  lazy val contentType:   Option[ContentType]   = findHeaderValue(Header.CONTENT_TYPE)
+  lazy val contentLength: Option[ContentLength] = findHeaderValue(Header.CONTENT_LENGTH).flatMap(_.toLongOption)
 
   /** Based on the name of the header, get the value associated with it.
     * @param name
@@ -24,7 +40,7 @@ class ServerRequest[F[_], T](request: Request[F], val param: T):
     * @return
     *   Http Request Header value
     */
-  def findHeaderValue(name: String): Option[String] = headers.find(_.is(name)).map(_.value)
+  def findHeaderValue(name: String): Option[ContentType] = headers.find(_.is(name)).map(_.value)
 
-  def as[A](using F: cats.MonadThrow[F], decoder: org.http4s.EntityDecoder[F, A]): F[A] =
+  def as[A](using MonadThrow[F], EntityDecoder[F, A]): F[A] =
     request.as[A]
