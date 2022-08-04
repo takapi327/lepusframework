93b04eb2c9fed0fe79878b014e44e50153c72a59
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-08-04 23:24:45 +0900

    Create Tag model

--- core/lepus-router/src/main/scala/lepus/router/RouterConstructor.scala
+++ core/lepus-router/src/main/scala/lepus/router/RouterConstructor.scala
@@ -61,19 +61,6 @@ abstract class RouterConstructor[F[_], P](using Async[F], Sync[F]):
   /** The value that will be the path of the Http request. */
   def endpoint: RequestEndpoint.Endpoint
 
-  /** Summary of this endpoint, used during Open API document generation. */
-  def summary: Option[String] = None
-
-  /** Description of this endpoint, used during Open API document generation. */
-  def description: Option[String] = None
-
-  /** Tag of this endpoint, used during Open API document generation. */
-  def tags: Set[Tag] = Set.empty[Tag]
-
-  /** A flag used during Open API document generation to indicate whether this endpoint is deprecated or not.
-    */
-  def deprecated: Option[Boolean] = None
-
   def requestBodies: Http[Request.Body[?]] = PartialFunction.empty
 
   /** An array of responses returned by each method. */
