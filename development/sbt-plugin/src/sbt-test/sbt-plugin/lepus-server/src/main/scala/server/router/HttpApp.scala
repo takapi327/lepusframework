8dc1ee09d029047c02f18f5ea86e8e0d1afd8196
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-08-12 22:11:36 +0900

    Fix HttpApp

--- development/sbt-plugin/src/sbt-test/sbt-plugin/lepus-server/src/main/scala/server/router/HttpApp.scala
+++ development/sbt-plugin/src/sbt-test/sbt-plugin/lepus-server/src/main/scala/server/router/HttpApp.scala
@@ -7,11 +7,27 @@
 package server.router
 
 import cats.effect.IO
-import cats.data.NonEmptyList
 
-import lepus.router.*
+import lepus.router.{ *, given }
+import lepus.router.model.{ Schema, ServerResponse }
+import lepus.router.http.Method
+import lepus.router.generic.semiauto.*
+
+case class Hello(name: String)
+object Hello:
+  given Schema[Hello] = deriveSchemer
+
+case class World(name: String)
+object World:
+  given Schema[World] = deriveSchemer
+
+object HelloWorldController:
+  def get = IO(ServerResponse.NoContent)
 
 object HttpApp extends RouterProvider[IO]:
 
-  override def routes: NonEmptyList[RouterConstructor[IO, _]] =
-    NonEmptyList.of(HelloRoute)
+  override def routes = combine {
+    "hello" / bindPath[String]("name") -> Router.of[IO, Hello, World, String] {
+      case Method.Get => HelloWorldController.get
+    }
+  }
