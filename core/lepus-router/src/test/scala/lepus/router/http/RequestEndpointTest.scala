38091f391c0b5414a7bdc4d04a3cc6056d909425
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-08-16 17:23:01 +0900

    Create RequestEndpoint test

---
+++ core/lepus-router/src/test/scala/lepus/router/http/RequestEndpointTest.scala
@@ -0,0 +1,59 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus.router.http
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class RequestEndpointTest extends AnyFlatSpec:
+
+  it should "generate endpoint" in {
+    assertCompiles("""
+      import lepus.router.{ *, given }
+      import lepus.router.http.RequestEndpoint.*
+
+      val endpoint1: Endpoint[String] = "test1" / bindPath[String]("p1")
+      val endpoint2: Endpoint[String] = bindPath[String]("p1") / "test2"
+      val endpoint3: Endpoint[(String, String)] = endpoint1 and endpoint2
+      val endpoint4: Endpoint[(String, String, Long)] = endpoint1 and endpoint2 / bindPath[Long]("p1")
+    """.stripMargin)
+  }
+
+  it should "generate endpoint failure" in {
+    assertDoesNotCompile("""
+      import lepus.router.{ *, given }
+      import lepus.router.http.RequestEndpoint.*
+
+      val endpoint1: Endpoint[Long] = "test1" / bindPath[String]("p1")
+      val endpoint2: Endpoint[Long] = bindPath[String]("p1") / "test2"
+      val endpoint3: Endpoint[(Long, Long)] = endpoint1 and endpoint2
+      val endpoint4: Endpoint[(Long, Long, String)] = endpoint1 and endpoint2 / bindPath[Long]("p1")
+    """.stripMargin)
+  }
+
+  it should "compile" in {
+    assertCompiles("""
+      import cats.effect.IO
+      import lepus.router.{ *, given }
+
+      bindPath[Long]("p1") / bindPath[String]("p2") -> new RouterConstructor[IO, (Long, String)]():
+        override def endpoint = bindPath[Long]("p1") / bindPath[String]("p2")
+        override def routes: HttpRoutes[IO, (Long, String)] = {
+          case GET => req => IO(lepus.router.model.ServerResponse.NoContent)
+        }
+    """.stripMargin)
+  }
+
+  it should "compile failure" in {
+    assertDoesNotCompile("""
+      import cats.effect.IO
+      import lepus.router.{ *, given }
+
+      bindPath[Long]("p1") / bindPath[String]("p2") -> new RouterConstructor[IO, (String, String)]():
+        override def endpoint = bindPath[Long]("p1") / bindPath[String]("p2")
+        override def routes: HttpRoutes[IO, (String, String)] = {
+          case GET => req => IO(lepus.router.model.ServerResponse.NoContent)
+        }
+    """.stripMargin)
+  }
