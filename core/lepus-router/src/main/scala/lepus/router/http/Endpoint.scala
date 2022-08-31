08e56b1d693b04c125b3ae53ff7c400ce3df3cef
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-08-31 21:12:46 +0900

    Add comment

--- core/lepus-router/src/main/scala/lepus/router/http/Endpoint.scala
+++ core/lepus-router/src/main/scala/lepus/router/http/Endpoint.scala
@@ -23,16 +23,45 @@ sealed trait Endpoint[T]:
 
 object Endpoint:
 
+  /** Model for representing endpoint parameters
+    *
+    * @tparam T
+    *   Type of parameters received from the URL
+    */
   sealed trait Param[T] extends Endpoint[T]:
-    def converter:   EndpointConverter[String, T]
+    /** A value to convert from a string to an arbitrary type.
+      */
+    def converter: EndpointConverter[String, T]
+
+    /** Details of this parameter
+      */
     def description: Option[String]
+
+    /** Indicates whether this parameter is required or not.
+      */
     def required: Boolean
+
+    /** Update parameter description
+      */
     def setDescription(content: String): ThisType
+
+    /** Update parameter required
+      */
     def setRequired(bool: Boolean): ThisType
 
+  /** Model for representing endpoint path parameters
+    *
+    * @tparam T
+    *   Type of parameters received from the URL
+    */
   sealed trait Path[T] extends Endpoint[T]:
     def name: String
 
+  /** Model for representing endpoint query parameters
+    *
+    * @tparam T
+    *   Type of parameters received from the URL
+    */
   sealed trait Query[T] extends Endpoint[T]:
     def key: String
 
@@ -65,7 +94,7 @@ object Endpoint:
             Param[T]:
     override private[lepus] type ThisType = PathParam[T]
     override def setDescription(content: String): PathParam[T] = this.copy(description = Some(content))
-    override def setRequired(bool: Boolean): PathParam[T] = this.copy(required = bool)
+    override def setRequired(bool: Boolean):      PathParam[T] = this.copy(required = bool)
     def validate(validator: Validator): Path[T] = ValidatePathParam(name, converter, validator, required, description)
 
   /** Query parameter
@@ -86,7 +115,7 @@ object Endpoint:
             Param[T]:
     override private[lepus] type ThisType = QueryParam[T]
     override def setDescription(content: String): QueryParam[T] = this.copy(description = Some(content))
-    override def setRequired(bool: Boolean): QueryParam[T] = this.copy(required = bool)
+    override def setRequired(bool: Boolean):      QueryParam[T] = this.copy(required = bool)
     def validate(validator: Validator): Query[T] = ValidateQueryParam(key, converter, validator, required, description)
 
   /** Validation defined value for dynamically changing path parameters
@@ -110,7 +139,7 @@ object Endpoint:
             Param[T]:
     override private[lepus] type ThisType = ValidatePathParam[T]
     override def setDescription(content: String): ValidatePathParam[T] = this.copy(description = Some(content))
-    override def setRequired(bool: Boolean): ValidatePathParam[T] = this.copy(required = bool)
+    override def setRequired(bool: Boolean):      ValidatePathParam[T] = this.copy(required = bool)
 
   /** Validation defined value for dynamically changing query parameters
     *
@@ -133,7 +162,7 @@ object Endpoint:
             Param[T]:
     override private[lepus] type ThisType = ValidateQueryParam[T]
     override def setDescription(content: String): ValidateQueryParam[T] = this.copy(description = Some(content))
-    override def setRequired(bool: Boolean): ValidateQueryParam[T] = this.copy(required = bool)
+    override def setRequired(bool: Boolean):      ValidateQueryParam[T] = this.copy(required = bool)
 
   /** Model to store Endpoint pairs
     *
