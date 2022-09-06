23cfa50fa1002b47c3f396c0bcf9512d68c8dae3
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-09-07 01:12:35 +0900

    Create LepusPrinter

---
+++ core/lepus-logger/src/main/scala/lepus/logger/LepusPrinter.scala
@@ -0,0 +1,42 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus.logger
+
+import org.legogroup.woof.{ ColorPrinter, EpochMillis, LogInfo, LogLevel, defaultTimeFormat }
+import org.legogroup.woof.ColorPrinter.Theme
+
+case class LepusPrinter(
+  theme:      Theme                 = Theme.defaultTheme,
+  formatTime: EpochMillis => String = defaultTimeFormat,
+) extends ColorPrinter(theme, formatTime):
+
+  def toPrint(
+    epochMillis: EpochMillis,
+    level:       LogLevel,
+    info:        LogInfo,
+    message:     String,
+    context:     List[(String, String)],
+    exception:   Throwable
+   ): String =
+    val levelColor   = theme.levelFormat(level)
+    val postfixColor = theme.postfixFormat
+    val reset        = theme.reset
+    val prefix       = level.productPrefix.toUpperCase.padTo(5, ' ')
+    val time         = formatTime(epochMillis)
+    val contextPart  =
+      if context.isEmpty then ""
+      else context.map(
+        (key, value) => s"${theme.contextKey}$key${theme.reset}=${theme.contextValue}$value"
+      ).mkString(", ") + theme.reset.getCode
+    s"""
+      |$time $levelColor[$prefix]$reset$contextPart $postfixColor${info.prefix}$reset: $message $postfixColor${info.postfix}$reset
+      |${exception.getMessage}
+      |${stackTrace(exception.getStackTrace)}
+    """.stripMargin
+
+  private def stackTrace(traces: Array[StackTraceElement]): String =
+    traces.map { trace =>
+      s"${trace.getClassName}.${trace.getMethodName} [${trace.getFileName}:${trace.getLineNumber}]"
+    }.mkString("\n")
