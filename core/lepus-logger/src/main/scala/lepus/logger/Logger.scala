8b00c29fc1a3a8c3304aff4ee2a701b8975ca49e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-09-11 19:13:14 +0900

    Add comment

--- core/lepus-logger/src/main/scala/lepus/logger/Logger.scala
+++ core/lepus-logger/src/main/scala/lepus/logger/Logger.scala
@@ -13,8 +13,14 @@ import org.legogroup.woof.*
 import org.legogroup.woof.Logger as WoofLogger
 import org.legogroup.woof.Logger.StringLocal
 
-// copied from woof:
-// https://github.com/LEGO/woof/blob/main/modules/core/shared/src/main/scala/org/legogroup/woof/Logger.scala
+/**
+ * An object that extends woof's Logger.
+ * copied from woof:
+ * https://github.com/LEGO/woof/blob/main/modules/core/shared/src/main/scala/org/legogroup/woof/Logger.scala
+ *
+ * @tparam F
+ *   the effect type.
+ */
 trait Logger[F[_]: StringLocal: Monad: Clock](
   output:  Output[F],
   outputs: Output[F]*
@@ -25,6 +31,18 @@ trait Logger[F[_]: StringLocal: Monad: Clock](
   val printer:     LepusPrinter   = summon[LepusPrinter]
   val filter:      Filter         = summon[Filter]
 
+  /**
+   * Methods for converting multiple arguments to strings for writing to the log.
+   *
+   * @param level
+   * Log level
+   * @param info
+   * Information on the part of the log that was spit out
+   * @param message
+   * log-displaying message
+   * @param context
+   * IOLocal internal storage
+   */
   private[lepus] def makeLogString(
     level:   LogLevel,
     info:    LogInfo,
@@ -33,8 +51,23 @@ trait Logger[F[_]: StringLocal: Monad: Clock](
   ): F[String] =
     Clock[F].realTime
       .map(d => EpochMillis(d.toMillis))
-      .map(now => summon[LepusPrinter].toPrint(now, level, info, message, context))
+      .map(now => printer.toPrint(now, level, info, message, context))
 
+  /**
+   * Methods for converting multiple arguments to strings for writing to the log.
+   *
+   * @param level
+   * Log level
+   * @param info
+   * Information on the part of the log that was spit out
+   * @param message
+   * log-displaying message
+   * @param context
+   * IOLocal internal storage
+   * @param exception
+   * exception information
+   * @return
+   */
   private[lepus] def makeLogString(
     level:     LogLevel,
     info:      LogInfo,
@@ -44,33 +77,64 @@ trait Logger[F[_]: StringLocal: Monad: Clock](
   ): F[String] =
     Clock[F].realTime
       .map(d => EpochMillis(d.toMillis))
-      .map(now => summon[LepusPrinter].toPrint(now, level, info, message, context, exception))
+      .map(now => printer.toPrint(now, level, info, message, context, exception))
 
+  /**
+   * Methods for processing to be written to the log.
+   *
+   * @param level
+   * Log level
+   * @param s
+   * A string formatted for writing to the log.
+   */
   private[lepus] def doOutputs(level: LogLevel, s: String): F[Unit] =
     val allOutputs = outputs.prepended(output)
     level match
       case LogLevel.Error => allOutputs.traverse_(_.outputError(s))
       case _              => allOutputs.traverse_(_.output(s))
 
+  /**
+   * Methods for processing internal storage and formatting and writing logs.
+   *
+   * @param level
+   * Log level
+   * @param message
+   * log-displaying message
+   * @param logInfo
+   * Information on the part of the log that was spit out
+   */
   override def doLog(level: LogLevel, message: String)(using logInfo: LogInfo): F[Unit] =
     for
-      context <- summon[StringLocal[F]].ask
+      context <- stringLocal.ask
       logLine <- makeLogString(level, logInfo, message, context)
-      _       <- doOutputs(level, logLine).whenA(summon[Filter](LogLine(level, logInfo, logLine, context)))
+      _       <- doOutputs(level, logLine).whenA(filter(LogLine(level, logInfo, logLine, context)))
     yield ()
 
+  /**
+   * Methods for processing internal storage and formatting and writing logs.
+   *
+   * @param level
+   * Log level
+   * @param message
+   * log-displaying message
+   * @param exception
+   * exception information
+   * @param logInfo
+   * Information on the part of the log that was spit out
+   */
   def doLog(level: LogLevel, message: String, exception: Throwable)(using logInfo: LogInfo): F[Unit] =
     for
-      context <- summon[StringLocal[F]].ask
+      context <- stringLocal.ask
       logLine <- makeLogString(level, logInfo, message, context, exception)
-      _       <- doOutputs(level, logLine).whenA(summon[Filter](LogLine(level, logInfo, logLine, context)))
+      _       <- doOutputs(level, logLine).whenA(filter(LogLine(level, logInfo, logLine, context)))
     yield ()
 
-  def debug(t: Throwable)(message: => String)(using LogInfo): F[Unit] = doLog(LogLevel.Debug, message, t)
-  def error(t: Throwable)(message: => String)(using LogInfo): F[Unit] = doLog(LogLevel.Error, message, t)
-  def info(t: Throwable)(message: => String)(using LogInfo):  F[Unit] = doLog(LogLevel.Info, message, t)
-  def trace(t: Throwable)(message: => String)(using LogInfo): F[Unit] = doLog(LogLevel.Trace, message, t)
-  def warn(t: Throwable)(message: => String)(using LogInfo):  F[Unit] = doLog(LogLevel.Warn, message, t)
+  /** A method that allows you to specify the level of logging by the method name. */
+  def debug(message: => String)(t: Throwable)(using LogInfo): F[Unit] = doLog(LogLevel.Debug, message, t)
+  def error(message: => String)(t: Throwable)(using LogInfo): F[Unit] = doLog(LogLevel.Error, message, t)
+  def info(message: => String)(t: Throwable)(using LogInfo):  F[Unit] = doLog(LogLevel.Info, message, t)
+  def trace(message: => String)(t: Throwable)(using LogInfo): F[Unit] = doLog(LogLevel.Trace, message, t)
+  def warn(message: => String)(t: Throwable)(using LogInfo):  F[Unit] = doLog(LogLevel.Warn, message, t)
 
 object Logger:
   def apply[F[_]: StringLocal: Monad: Clock](
