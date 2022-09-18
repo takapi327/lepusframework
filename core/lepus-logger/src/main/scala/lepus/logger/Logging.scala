2a5455c01f3d16453e5f5eb80ff5b2a0f4c12e62
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-09-18 22:45:10 +0900

    Create Logging

---
+++ core/lepus-logger/src/main/scala/lepus/logger/Logging.scala
@@ -0,0 +1,45 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus.logger
+
+import cats.Monad
+import cats.syntax.all.*
+
+import cats.effect.kernel.Clock
+import cats.effect.std.Console
+
+trait Logging[F[_]]:
+
+  def output:    Output[F]
+  def filter:    Filter
+  def formatter: Formatter
+
+  def logger: LoggerF[F]
+
+trait LoggingIO[F[_]: Monad: Clock: Console] extends Logging[F]:
+
+  override val output: Output[F] = ConsoleOutput[F]
+  override val filter: Filter = Filter.everything
+  override val formatter: Formatter = DefaultFormatter
+
+  val logger: LoggerF[F] = new LoggerF[F]:
+
+    private def buildLogMessage[M](level: Level, msg: => M, ex: Option[Throwable], ctx: Map[String, String]): Execute[F, LogMessage] =
+      Clock[F].realTime.map(now =>
+        LogMessage(level, msg.toString, summon[ExecLocation], ctx, ex, Thread.currentThread().getName, now.toMillis)
+      )
+
+    private def doOutput(msg: LogMessage): Execute[F, Unit] =
+      (msg.level, msg.exception) match
+        case (Level.Error, Some(ex)) => output.outputError(formatter.format(msg)) >> output.outputStackTrace(ex)
+        case (Level.Error, None)     => output.outputError(formatter.format(msg))
+        case (_, Some(ex))           => output.output(formatter.format(msg)) >> output.outputStackTrace(ex)
+        case _                       => output.output(formatter.format(msg))
+
+    override protected def log[M](level: Level, msg: => M, ex: Option[Throwable], ctx: Map[String, String]): Execute[F, Unit] =
+      buildLogMessage(level, msg, ex, ctx).flatMap(log)
+
+    override protected def log(msg: LogMessage): Execute[F, Unit] =
+      doOutput(msg).whenA(filter(msg))
