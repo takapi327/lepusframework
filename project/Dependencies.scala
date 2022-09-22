7d25aaedb1ff3c63f431eede1fef63160731ff6a
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-09-22 15:00:10 +0900

    update version 1.4.1 -> 1.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
   val logback = "ch.qos.logback" % "logback-classic" % "1.4.0"
 
-  val typesafeConfig = "com.typesafe" % "config" % "1.4.1"
+  val typesafeConfig = "com.typesafe" % "config" % "1.4.2"
 
   val reflect = "org.scala-lang" % "scala-reflect" % "2.13.8"
 
