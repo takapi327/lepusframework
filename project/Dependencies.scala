ce43af7016602021b805f197b110f9f017c10f8c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-09-22 15:12:28 +0900

    update scalatest version 3.2.11 -> 3.2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
     "specs2-junit",
   ).map("org.specs2" %% _ % specs2Version % Test)
 
-  val scalaTest  = "org.scalatest"     %% "scalatest"       % "3.2.11"   % Test
+  val scalaTest  = "org.scalatest"     %% "scalatest"       % "3.2.12"   % Test
   val scalaCheck = "org.scalacheck"    %% "scalacheck"      % "1.15.4"   % Test
   val scalaPlus  = "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0" % Test
 
