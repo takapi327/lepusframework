5dd39b1bcf35f3fb1ac6bd9613eef75b0ebb7571
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-09-22 13:36:27 +0900

    Create JsonFormatter

---
+++ core/lepus-logger/src/main/scala/lepus/logger/JsonFormatter.scala
@@ -0,0 +1,24 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus.logger
+
+import io.circe.Json
+import io.circe.syntax.*
+
+object JsonFormatter extends Formatter:
+  override def format(msg: LogMessage): String =
+    val timestamp = Formatter.formatTimestamp(msg.timestamp)
+    val context   = Formatter.formatCtx(msg.context)
+    val fileName  = s"${ msg.execLocation.fileName }:${ msg.execLocation.lineNumber }"
+    val json      = Json.obj(
+      "timestamp"     -> timestamp.asJson,
+      "level"         -> msg.level.toString.asJson,
+      "threadName"    -> msg.threadName.asJson,
+      "enclosureName" -> msg.execLocation.enclosureName.asJson,
+      "message"       -> msg.message.asJson,
+      "fileName"      -> fileName.asJson,
+      "context"       -> context.asJson
+    )
+    json.toString
