5bda1e01326fa9a3df3aa2fd5faf822e76c74a4b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-09-23 17:30:28 +0900

    Create ProcessState

---
+++ development/sbt-plugin/src/main/scala/lepus/sbt/ProcessState.scala
@@ -0,0 +1,38 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus.sbt
+
+import java.util.concurrent.atomic.AtomicReference
+
+import scala.annotation.tailrec
+
+import sbt.ProjectRef
+
+case class ProcessState(processes: Map[ProjectRef, AppProcess]) {
+  def updateProcesses(projectRef: ProjectRef, process: AppProcess): ProcessState =
+    copy(processes + (projectRef -> process))
+  def removeProcess(projectRef: ProjectRef): ProcessState =
+    processes.get(projectRef) match {
+      case Some(_) => copy(processes - projectRef)
+      case None    => this
+    }
+  def getProcess(projectRef: ProjectRef): Option[AppProcess] =
+    processes.get(projectRef)
+}
+
+object ProcessState {
+  def initial: ProcessState = ProcessState(Map.empty)
+
+  private val state = new AtomicReference(initial)
+
+  @tailrec def update(action: ProcessState => ProcessState): ProcessState = {
+    val oldState = state.get()
+    val newState = action(oldState)
+    if (!state.compareAndSet(oldState, newState)) update(action)
+    else newState
+  }
+
+  def get: ProcessState = state.get()
+}
