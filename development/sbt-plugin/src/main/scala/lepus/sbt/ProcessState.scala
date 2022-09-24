f8195b5e9179bb2165d3fc70baecbcfde07e94ae
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-09-24 11:49:47 +0900

    Add comment

--- development/sbt-plugin/src/main/scala/lepus/sbt/ProcessState.scala
+++ development/sbt-plugin/src/main/scala/lepus/sbt/ProcessState.scala
@@ -10,23 +10,52 @@ import scala.annotation.tailrec
 
 import sbt.ProjectRef
 
+/** Class for managing application processes with AtomicReference.
+  *
+  * @param processes
+  *   Map with sbt project as key and application process as value.
+  */
 case class ProcessState(processes: Map[ProjectRef, AppProcess]) {
+
+  /** Update the application process corresponding to the sbt project.
+    *
+    * @param projectRef
+    *   Class for uniquely referencing a project by URI and project identifier (String).
+    * @param process
+    *   Class for managing application processes.
+    */
   def updateProcesses(projectRef: ProjectRef, process: AppProcess): ProcessState =
     copy(processes + (projectRef -> process))
+
+  /** Remove the application process corresponding to the sbt project.
+    *
+    * @param projectRef
+    *   Class for uniquely referencing a project by URI and project identifier (String).
+    */
   def removeProcess(projectRef: ProjectRef): ProcessState =
     processes.get(projectRef) match {
       case Some(_) => copy(processes - projectRef)
       case None    => this
     }
+
+  /** Get the application process corresponding to the sbt project.
+    *
+    * @param projectRef
+    *   Class for uniquely referencing a project by URI and project identifier (String).
+    */
   def getProcess(projectRef: ProjectRef): Option[AppProcess] =
     processes.get(projectRef)
 }
 
 object ProcessState {
+
+  /** A method to initialize the application process with empty. */
   def initial: ProcessState = ProcessState(Map.empty)
 
+  /** Variable for storing the application process in AtomicReference. */
   private val state = new AtomicReference(initial)
 
+  /** Methods for updating from the old State to the new State. */
   @tailrec def update(action: ProcessState => ProcessState): ProcessState = {
     val oldState = state.get()
     val newState = action(oldState)
@@ -34,5 +63,6 @@ object ProcessState {
     else newState
   }
 
+  /** Alias to get the value of State. */
   def get: ProcessState = state.get()
 }
