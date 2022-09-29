2f54b909319ea64b25ca8e113148f832eebc6132
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-09-29 23:29:13 +0900

    Create DataSource

---
+++ core/lepus-doobie/src/main/scala/lepus/doobie/DataSource.scala
@@ -0,0 +1,28 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus.doobie
+
+case class DataSource(
+  path:     String,
+  hostspec: String,
+  database: String
+):
+
+  override def toString: String = s"$path://$hostspec/$database"
+
+object DataSource:
+
+  val SYNTAX_DATA_SOURCE = """^([.\w]+)://(\w+?)/(\w+)$""".r
+
+  def apply(str: String): DataSource = str match
+    case SYNTAX_DATA_SOURCE(path, hostspec, database) => DataSource(path, hostspec, database)
+    case _ => throw new IllegalArgumentException(
+      s"""
+         |$str does not match DataSource format
+         |
+         |example:
+         |  path://hostspec/database
+         |""".stripMargin
+    )
