ab563a47cb9eb4108e9262f92d4e24cab1628137
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-09-29 21:40:40 +0900

    Action sbt scalafmtAll

--- core/lepus-doobie/src/main/scala/lepus/doobie/DoobieLogHandler.scala
+++ core/lepus-doobie/src/main/scala/lepus/doobie/DoobieLogHandler.scala
@@ -1,6 +1,6 @@
 /** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  * file that was distributed with this source code.
+  */
 
 package lepus.doobie
 
@@ -21,31 +21,31 @@ trait DoobieLogHandler[F[_]](using ExecLocation):
         self.logger.warn(
           s"""Slow query (execution: $exec, processing: $processing):
              |
-             | ${sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
+             | ${ sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ") }
              |
-             | arguments = [${args.mkString(", ")}]
-             |   elapsed = ${exec.toMillis} ms exec + ${processing.toMillis} ms processing (${(exec + processing).toMillis} ms total)
+             | arguments = [${ args.mkString(", ") }]
+             |   elapsed = ${ exec.toMillis } ms exec + ${ processing.toMillis } ms processing (${ (exec + processing).toMillis } ms total)
              |""".stripMargin
         )
       else
         self.logger.info(
           s"""Successful Statement Execution:
              |
-             | ${sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
+             | ${ sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ") }
              |
-             | arguments = [${args.mkString(", ")}]
-             |   elapsed = ${exec.toMillis} ms exec + ${processing.toMillis} ms processing (${(exec + processing).toMillis} ms total)
+             | arguments = [${ args.mkString(", ") }]
+             |   elapsed = ${ exec.toMillis } ms exec + ${ processing.toMillis } ms processing (${ (exec + processing).toMillis } ms total)
              |""".stripMargin
         )
     case ProcessingFailure(sql, args, exec, processing, failure) =>
       self.logger.error(
         s"""Failed Result Processing:
            |
-           | ${sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
+           | ${ sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ") }
            |
-           | arguments = [${args.mkString(", ")}]
-           |   elapsed = ${exec.toMillis} ms exec + ${processing.toMillis} ms processing (failed) (${(exec + processing).toMillis} ms total)
-           |   failure = ${failure.getMessage}
+           | arguments = [${ args.mkString(", ") }]
+           |   elapsed = ${ exec.toMillis } ms exec + ${ processing.toMillis } ms processing (failed) (${ (exec + processing).toMillis } ms total)
+           |   failure = ${ failure.getMessage }
            |""".stripMargin,
         failure
       )
@@ -53,11 +53,11 @@ trait DoobieLogHandler[F[_]](using ExecLocation):
       self.logger.error(
         s"""Failed Statement Execution:
            |
-           | ${sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ")}
+           | ${ sql.linesIterator.dropWhile(_.trim.isEmpty).mkString("\n  ") }
            |
-           | arguments = [${args.mkString(", ")}]
-           |   elapsed = ${exec.toMillis} ms exec (failed)
-           |   failure = ${failure.getMessage}
+           | arguments = [${ args.mkString(", ") }]
+           |   elapsed = ${ exec.toMillis } ms exec (failed)
+           |   failure = ${ failure.getMessage }
            |""".stripMargin,
         failure
       )
