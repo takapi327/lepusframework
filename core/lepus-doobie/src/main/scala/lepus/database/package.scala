fbbc6a1f8e35739998dec1e6383941ce1e635b33
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-04 00:42:16 +0900

    Create package

---
+++ core/lepus-doobie/src/main/scala/lepus/database/package.scala
@@ -0,0 +1,10 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus
+
+import doobie.Transactor
+
+package object database extends LepusDoobie:
+  type DBTransactor[F[_]] = Map[DatabaseConfig, Transactor[F]]
