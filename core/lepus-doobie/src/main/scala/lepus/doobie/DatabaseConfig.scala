56b0bf4c255970d90bf6573b649407d43eb89cbb
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-03 22:09:54 +0900

    Fix DatabaseConfig

--- core/lepus-doobie/src/main/scala/lepus/doobie/DatabaseConfig.scala
+++ core/lepus-doobie/src/main/scala/lepus/doobie/DatabaseConfig.scala
@@ -4,20 +4,29 @@
 
 package lepus.doobie
 
-case class DataSource(
+case class DatabaseConfig(
   path:     String,
   hostspec: String,
   database: String
 ):
 
+  override final def equals(other: Any): Boolean = other match
+    case that: DatabaseConfig =>
+      (that _equal this) &&
+        (this.path     == that.path)     &&
+        (this.hostspec == that.hostspec) &&
+        (this.database == that.database)
+    case _ => false
+  private def _equal(other: Any) = other.isInstanceOf[DatabaseConfig]
+
   override def toString: String = s"$path://$hostspec/$database"
 
-object DataSource:
+object DatabaseConfig:
 
   val SYNTAX_DATA_SOURCE = """^([.\w]+)://(\w+?)/(\w+)$""".r
 
-  def apply(str: String): DataSource = str match
-    case SYNTAX_DATA_SOURCE(path, hostspec, database) => DataSource(path, hostspec, database)
+  def apply(str: String): DatabaseConfig = str match
+    case SYNTAX_DATA_SOURCE(path, hostspec, database) => DatabaseConfig(path, hostspec, database)
     case _ =>
       throw new IllegalArgumentException(
         s"""
@@ -28,4 +37,4 @@ object DataSource:
          |""".stripMargin
       )
 
-type DataSourceCF[T] = DataSource ?=> T
+type DatabaseCF[T] = DatabaseConfig ?=> T
