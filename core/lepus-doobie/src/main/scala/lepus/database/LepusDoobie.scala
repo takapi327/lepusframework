d64874809285fc62b6f275e3213709acb3aadd62
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-04 00:41:59 +0900

    Create LepusDoobie

---
+++ core/lepus-doobie/src/main/scala/lepus/database/LepusDoobie.scala
@@ -0,0 +1,14 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus.database
+
+trait LepusDoobie
+  extends doobie.Aliases,
+    doobie.hi.Modules,
+    doobie.free.Modules,
+    doobie.free.Types,
+    doobie.util.meta.LegacyInstantMetaInstance,
+    doobie.free.Instances,
+    doobie.syntax.AllSyntax
