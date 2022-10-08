8b4a130a60eab6fe843743caba59bd17020f448c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-08 21:57:55 +0900

    Add method

--- core/lepus-database/src/main/scala/lepus/database/DoobieRepository.scala
+++ core/lepus-database/src/main/scala/lepus/database/DoobieRepository.scala
@@ -4,16 +4,24 @@
 
 package lepus.database
 
+import scala.annotation.targetName
+
+import cats.data.{ OptionT, EitherT, Kleisli }
+
 import cats.effect.Async
+
+import fs2.{ Pipe, Stream }
 
 import doobie.Transactor
 import doobie.util.log.LogHandler
+import doobie.util.fragment.Fragment
 
 trait DoobieRepository[F[_]: Async](using DBTransactor[F]) extends DoobieLogHandler:
 
   given LogHandler = logHandler
 
   def database: DatabaseConfig
+  def table: String
 
   private val connection: Option[Transactor[F]] = summon[DBTransactor[F]]
     .flatMap((db, xa) => {
@@ -21,8 +29,15 @@ trait DoobieRepository[F[_]: Async](using DBTransactor[F]) extends DoobieLogHand
     })
     .headOption
 
+  def select(params: String*): Fragment =
+    fr"SELECT" ++ Fragment.const(params.mkString(",")) ++ fr"FROM" ++ Fragment.const(table)
+
   object Action:
-    def apply[T](func: Transactor[F] => F[T]): F[T] =
+    @targetName("default") def apply[T](func: Transactor[F] => F[T]): F[T] =
+      connection match
+        case Some(transactor) => func(transactor)
+        case None             => throw new IllegalStateException(s"$database database is not registered.")
+    @targetName("defaultK") def apply[K[_], T](func: Transactor[F] => K[T]): K[T] =
       connection match
         case Some(transactor) => func(transactor)
         case None             => throw new IllegalStateException(s"$database database is not registered.")
