d2531728649af21a022df1b68e435b65faccf6b1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-08 22:55:28 +0900

    Add transact method

--- core/lepus-database/src/main/scala/lepus/database/DoobieRepository.scala
+++ core/lepus-database/src/main/scala/lepus/database/DoobieRepository.scala
@@ -29,8 +29,7 @@ trait DoobieRepository[F[_]: Async](using DBTransactor[F]) extends DoobieLogHand
     })
     .headOption
 
-  def select(params: String*): Fragment =
-    fr"SELECT" ++ Fragment.const(params.mkString(",")) ++ fr"FROM" ++ Fragment.const(table)
+  def select(params: String*): LepusQuery.Select = LepusQuery.select(table, params*)
 
   object Action:
     @targetName("default") def apply[T](func: Transactor[F] => F[T]): F[T] =
@@ -40,4 +39,21 @@ trait DoobieRepository[F[_]: Async](using DBTransactor[F]) extends DoobieLogHand
     @targetName("defaultK") def apply[K[_], T](func: Transactor[F] => K[T]): K[T] =
       connection match
         case Some(transactor) => func(transactor)
+        case None             => throw new IllegalStateException(s"$database database is not registered.")
+
+    def transact[T](func: ConnectionIO[T]): F[T] =
+      this.apply(func.transact[F])
+    def transact[T](func: OptionT[ConnectionIO, T]): OptionT[F, T] =
+      this.apply[[A] =>> OptionT[F, A], T](func.transact[F])
+    def transact[T, E](func: EitherT[ConnectionIO, E, T]): EitherT[F, E, T] =
+      this.apply[[A] =>> EitherT[F, E, A], T](func.transact[F])
+    def transact[T, E](func: Kleisli[ConnectionIO, E, T]): Kleisli[F, E, T] =
+      this.apply[[A] =>> Kleisli[F, E, A], T](func.transact[F])
+    def transact[T](func: Stream[F, T])(using Stream[F, T] =:= Stream[ConnectionIO, T]): Stream[F, T] =
+      this.apply[[A] =>> Stream[F, A], T](func.transact[F])
+    def transact[A, B](func: Pipe[F, A, B])(using Pipe[F, A, B] =:= Pipe[ConnectionIO, A, B]): Pipe[F, A, B] =
+      this.apply[[T] =>> Pipe[F, T, B], A](func.transact[F])
+    def transact[A, B](func: Stream[[T] =>> Kleisli[ConnectionIO, A, T], B]): Stream[[T] =>> Kleisli[F, A, T], B] =
+      connection match
+        case Some(transactor) => func.transact[F](transactor)
         case None             => throw new IllegalStateException(s"$database database is not registered.")
