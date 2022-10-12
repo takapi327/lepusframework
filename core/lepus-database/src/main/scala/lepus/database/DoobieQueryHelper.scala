285beb711fcba18c6152ad444c6990637b04fb44
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-13 01:21:59 +0900

    Add comment

--- core/lepus-database/src/main/scala/lepus/database/DoobieQueryHelper.scala
+++ core/lepus-database/src/main/scala/lepus/database/DoobieQueryHelper.scala
@@ -9,15 +9,18 @@ import lepus.core.format.Naming
 
 trait DoobieQueryHelper extends SchemaHelper:
 
+  /** Name of the database table to use with this helper */
   def table: String
 
+  /** Convenience methods for SELECT statements to connect to databases. */
   def select(params: String*):           LepusQuery.Select = LepusQuery.select(table, params*)
   def select[T: Schema]:                 LepusQuery.Select = LepusQuery.select[T](table)
   def select[T: Schema](naming: Naming): LepusQuery.Select = LepusQuery.select[T](table, naming)
+
+  /** Convenience method for INSERT INTO statement to connect to a database. */
   def insert(params: String*):           LepusQuery.Insert = LepusQuery.insert(table, params*)
   def insert[T: Schema]:                 LepusQuery.Insert = LepusQuery.insert[T](table)
   def insert[T: Schema](naming: Naming): LepusQuery.Insert = LepusQuery.insert[T](table, naming)
-
   def insert[T: Write: Schema](value: T): ConnectionIO[Int] =
     update[T].run(value)
   def insert[T: Write: Schema](values: T*): ConnectionIO[Int] =
