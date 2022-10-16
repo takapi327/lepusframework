db1518760a04d3407cd02b4d1aff01bfe7340bfe
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-16 18:27:32 +0900

    Fix DoobieRepository

--- core/lepus-database/src/main/scala/lepus/database/DoobieRepository.scala
+++ core/lepus-database/src/main/scala/lepus/database/DoobieRepository.scala
@@ -4,55 +4,60 @@
 
 package lepus.database
 
-import scala.annotation.targetName
-
 import cats.data.{ OptionT, EitherT, Kleisli }
 
 import cats.effect.Async
+import cats.effect.kernel.MonadCancelThrow
 
 import fs2.{ Pipe, Stream }
 
-import doobie.Transactor
-import doobie.util.log.LogHandler
-import doobie.util.fragment.Fragment
+/** Trait to build Repository for DB connection using doobie.
+  *
+  * @tparam F
+  *   the effect type.
+  *
+  * example:
+  * {{{
+  *   import cats.effect.IO
+  *
+  *   import doobie.Transactor
+  *   import doobie.implicits.*
+  *
+  *   class TaskRepository(using: Transactor[IO]) extends DoobieRepository[IO]:
+  *
+  *     def getAll(): IO[List[Task]] =
+  *       sql"SELECT * FROM task".query[Task].to[List]
+  * }}}
+  */
+trait DoobieRepository[F[_]: Async: MonadCancelThrow](using xa: Transactor[F]) extends DoobieLogHandler:
 
-trait DoobieRepository[F[_]: Async](using dbt: DBTransactor[F]) extends DoobieLogHandler:
+  given [T]: Conversion[ConnectionIO[T], F[T]] with
+    override def apply(connection: ConnectionIO[T]): F[T] =
+      connection.transact(xa)
 
-  def database: DatabaseConfig
+  given [T]: Conversion[OptionT[ConnectionIO, T], OptionT[F, T]] with
+    override def apply(connection: OptionT[ConnectionIO, T]): OptionT[F, T] =
+      connection.transact(xa)
 
-  private val connection: Option[Transactor[F]] = dbt
-    .get(database)
-    .orElse(
-      dbt
-        .flatMap((db, xa) => {
-          if db equals database then Some(xa) else None
-        })
-        .headOption
-    )
+  given [T, E]: Conversion[EitherT[ConnectionIO, E, T], EitherT[F, E, T]] with
+    override def apply(connection: EitherT[ConnectionIO, E, T]): EitherT[F, E, T] =
+      connection.transact(xa)
 
-  object Action:
-    @targetName("default") def apply[T](func: Transactor[F] => F[T]): F[T] =
-      connection match
-        case Some(transactor) => func(transactor)
-        case None             => throw new IllegalStateException(s"$database database is not registered.")
-    @targetName("defaultK") def apply[K[_], T](func: Transactor[F] => K[T]): K[T] =
-      connection match
-        case Some(transactor) => func(transactor)
-        case None             => throw new IllegalStateException(s"$database database is not registered.")
+  given [T, E]: Conversion[Kleisli[ConnectionIO, E, T], Kleisli[F, E, T]] with
+    override def apply(connection: Kleisli[ConnectionIO, E, T]): Kleisli[F, E, T] =
+      connection.transact(xa)
 
-    def transact[T](func: ConnectionIO[T]): F[T] =
-      this.apply(func.transact[F])
-    def transact[T](func: OptionT[ConnectionIO, T]): OptionT[F, T] =
-      this.apply[[A] =>> OptionT[F, A], T](func.transact[F])
-    def transact[T, E](func: EitherT[ConnectionIO, E, T]): EitherT[F, E, T] =
-      this.apply[[A] =>> EitherT[F, E, A], T](func.transact[F])
-    def transact[T, E](func: Kleisli[ConnectionIO, E, T]): Kleisli[F, E, T] =
-      this.apply[[A] =>> Kleisli[F, E, A], T](func.transact[F])
-    def transact[T](func: Stream[F, T])(using Stream[F, T] =:= Stream[ConnectionIO, T]): Stream[F, T] =
-      this.apply[[A] =>> Stream[F, A], T](func.transact[F])
-    def transact[A, B](func: Pipe[F, A, B])(using Pipe[F, A, B] =:= Pipe[ConnectionIO, A, B]): Pipe[F, A, B] =
-      this.apply[[T] =>> Pipe[F, T, B], A](func.transact[F])
-    def transact[A, B](func: Stream[[T] =>> Kleisli[ConnectionIO, A, T], B]): Stream[[T] =>> Kleisli[F, A, T], B] =
-      connection match
-        case Some(transactor) => func.transact[F](transactor)
-        case None             => throw new IllegalStateException(s"$database database is not registered.")
+  given [T]: Conversion[Stream[ConnectionIO, T], Stream[F, T]] with
+    override def apply(connection: Stream[ConnectionIO, T]): Stream[F, T] =
+      connection.transact(xa)
+
+  given [A, B]: Conversion[Pipe[ConnectionIO, A, B], Pipe[F, A, B]] with
+    override def apply(connection: Pipe[ConnectionIO, A, B]): Pipe[F, A, B] =
+      connection.transact(xa)
+
+  given streamToKleisli[A, B]
+    : Conversion[Stream[[T] =>> Kleisli[ConnectionIO, A, T], B], Stream[[T] =>> Kleisli[F, A, T], B]] with
+    override def apply(
+      connection: Stream[[T] =>> Kleisli[ConnectionIO, A, T], B]
+    ): Stream[[T] =>> Kleisli[F, A, T], B] =
+      connection.transact(xa)
