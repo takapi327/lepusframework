30c22b0b85cdcedfb96d8a647e6de15c8404c77d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-16 18:28:23 +0900

    Action sbt scalafmtAll

--- core/lepus-database/src/main/scala/lepus/database/DatabaseModule.scala
+++ core/lepus-database/src/main/scala/lepus/database/DatabaseModule.scala
@@ -1,25 +1,24 @@
 /** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  * file that was distributed with this source code.
+  */
 
 package lepus.database
 
-/**
- * Module for implicitly passing the Transactor generated for each Database to DoobieRepository.
- *
- * @tparam F
- *   the effect type.
- *
- * example:
- * {{{
- *   case class ToDoDatabase(
- *     database: DatabaseConfig
- *   )(using DBTransactor[F]) extends DatabaseModule[F]:
- *
- *     val taskRepository     = new TaskRepository
- *     val categoryRepository = new CategoryRepository
- * }}}
- */
+/** Module for implicitly passing the Transactor generated for each Database to DoobieRepository.
+  *
+  * @tparam F
+  *   the effect type.
+  *
+  * example:
+  * {{{
+  *   case class ToDoDatabase(
+  *     database: DatabaseConfig
+  *   )(using DBTransactor[F]) extends DatabaseModule[F]:
+  *
+  *     val taskRepository     = new TaskRepository
+  *     val categoryRepository = new CategoryRepository
+  * }}}
+  */
 trait DatabaseModule[F[_]](using dbt: DBTransactor[F]):
 
   def database: DatabaseConfig
@@ -32,6 +31,7 @@ trait DatabaseModule[F[_]](using dbt: DBTransactor[F]):
           if db equals database then Some(xa) else None
         })
         .headOption
-    ).getOrElse(throw new IllegalStateException(s"$database database is not registered."))
+    )
+    .getOrElse(throw new IllegalStateException(s"$database database is not registered."))
 
   given Transactor[F] = transactor
