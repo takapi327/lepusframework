ede6e8fa5089a13d487c11909f4f3ad59507fb3e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-20 02:05:31 +0900

    Fix DatabaseModule with use DatabaseModule in DoobieRepository

--- core/lepus-database/src/main/scala/lepus/database/DatabaseModule.scala
+++ core/lepus-database/src/main/scala/lepus/database/DatabaseModule.scala
@@ -13,25 +13,32 @@ package lepus.database
   * {{{
   *   case class ToDoDatabase(
   *     database: DatabaseConfig
-  *   )(using DBTransactor[F]) extends DatabaseModule[F]:
-  *
-  *     val taskRepository     = new TaskRepository
-  *     val categoryRepository = new CategoryRepository
+  *   )(using DBTransactor[F]) extends DatabaseModule[F]
   * }}}
   */
 trait DatabaseModule[F[_]](using dbt: DBTransactor[F]):
 
   def database: DatabaseConfig
 
-  private val transactor: Transactor[F] = dbt
-    .get(database)
-    .orElse(
-      dbt
-        .flatMap((db, xa) => {
-          if db equals database then Some(xa) else None
-        })
-        .headOption
-    )
-    .getOrElse(throw new IllegalStateException(s"$database database is not registered."))
+  private[lepus] val transactor: Transactor[F] =
+    require(database.dataSource.length == 1, "If you are replicating to a database, you must specify the access point")
+    database.dataSource.headOption
+      .flatMap(ds => {
+        dbt
+          .get(ds)
+          .orElse(
+            dbt
+              .flatMap((db, xa) => {
+                if db equals ds then Some(xa) else None
+              })
+              .headOption
+          )
+      })
+      .getOrElse(throw new IllegalStateException(s"$database database is not registered."))
 
-  given Transactor[F] = transactor
+  private[lepus] def transactor(key: String): Transactor[F] = database.dataSource
+    .find(_.replication.contains(key))
+    .flatMap(ds => {
+      dbt.get(ds)
+    })
+    .getOrElse(throw new IllegalStateException(s"$database database is not registered."))
