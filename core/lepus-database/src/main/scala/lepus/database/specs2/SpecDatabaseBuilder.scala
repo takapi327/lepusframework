f15ea895cd8f703f23de2dbca725cda0fa97577d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-10-20 15:22:17 +0900

    Create SpecDatabaseBuilder

---
+++ core/lepus-database/src/main/scala/lepus/database/specs2/SpecDatabaseBuilder.scala
@@ -0,0 +1,18 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+  * file that was distributed with this source code.
+  */
+
+package lepus.database.specs2
+
+import cats.effect.{ Resource, Sync, Async }
+
+import lepus.database.*
+
+trait SpecDatabaseBuilder[F[_]: Sync: Async] extends DriverBuilder:
+
+  def database: DatabaseConfig
+
+  private def dataSources: DBTransactor[F] =
+    database.dataSource.map(ds => ds -> makeFromDataSource[F](ds)).toMap
+
+  given DBTransactor[F] = dataSources
