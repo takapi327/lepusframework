1df2a208354fc4d6bb2329b785bebb419827ef6c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-11-13 22:24:30 +0900

    Delete unused

--- core/lepus-doobie/src/main/scala/lepus/doobie/DatabaseModule.scala
+++ core/lepus-doobie/src/main/scala/lepus/doobie/DatabaseModule.scala
@@ -44,37 +44,5 @@ trait DatabaseModule[F[_]: Async](using context: LepusContext):
   private[lepus] val transactor: String => Transactor[F] = (key: String) =>
     database.dataSource
       .find(_.replication.contains(key))
-      .flatMap(ds => {
-        println("===============")
-        println("Start Generate Transactor")
-        println("===============")
-        context.get(ds).map((ec, datasource) => Transactor.fromDataSource[F](datasource, ec))
-      })
+      .flatMap(ds => context.get(ds).map((ec, datasource) => Transactor.fromDataSource[F](datasource, ec)))
       .getOrElse(throw new IllegalStateException(s"$database database is not registered."))
-
-  /** A method that tests the initialized database connection and attempts a wait connection at 5 second intervals until
-    * a connection is available.
-    *
-    * @param xa
-    *   A thin wrapper around a source of database connections, an interpreter, and a strategy for running programs,
-    *   parameterized over a target monad M and an arbitrary wrapped value A. Given a stream or program in ConnectionIO
-    *   or a program in Kleisli, a Transactor can discharge the doobie machinery and yield an effectful stream or
-    *   program in M.
-    */
-  // def testConnection(xa: Transactor[F]): F[Unit] =
-  //  (testQuery(xa) >> logger.info(s"$dataSource Database connection test complete")).onError { (ex: Throwable) =>
-  //    logger.warn(s"$dataSource Database not available, waiting 5 seconds to retry...", ex) >>
-  //      Sync[F].sleep(5.seconds) >>
-  //      testConnection(xa)
-  //  }
-
-  /** A query to be executed to check the connection to the database.
-    *
-    * @param xa
-    *   A thin wrapper around a source of database connections, an interpreter, and a strategy for running programs,
-    *   parameterized over a target monad M and an arbitrary wrapped value A. Given a stream or program in ConnectionIO
-    *   or a program in Kleisli, a Transactor can discharge the doobie machinery and yield an effectful stream or
-    *   program in M.
-    */
-  // def testQuery(xa: Transactor[F]): F[Unit] =
-  //  Sync[F].void(sql"select 1".query[Int].unique.transact(xa))
