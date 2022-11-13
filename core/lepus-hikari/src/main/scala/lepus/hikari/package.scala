69ba47644aa407af4e803602ffe6b1eb7cfd6986
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-11-13 22:52:08 +0900

    Create hikari package

---
+++ core/lepus-hikari/src/main/scala/lepus/hikari/package.scala
@@ -0,0 +1,12 @@
+/** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package lepus
+
+import com.zaxxer.hikari.HikariDataSource
+
+import lepus.database.LepusContext
+
+package object hikari:
+  type HikariContext = LepusContext[HikariDataSource]
