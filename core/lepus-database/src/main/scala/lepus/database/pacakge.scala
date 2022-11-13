abca15685b0d7e7758eac43277047c6e923123bc
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-11-13 22:53:17 +0900

    Add LepusContext type

--- core/lepus-database/src/main/scala/lepus/database/pacakge.scala
+++ core/lepus-database/src/main/scala/lepus/database/pacakge.scala
@@ -4,5 +4,8 @@
 
 package lepus
 
+import javax.sql.DataSource as JDataSource
+
 package object database:
   type DatabaseCF[T] = DataSource ?=> T
+  type LepusContext[T <: JDataSource] = Map[DataSource, DatabaseContext[T]]
