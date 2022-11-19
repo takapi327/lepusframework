64a7a8369efa80f8073b350f123c61522a175de8
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-11-19 15:04:20 +0900

    Action sbt scalafmtAll

--- core/lepus-doobie/src/main/scala/lepus/doobie/syntax/ConnectionIOOps.scala
+++ core/lepus-doobie/src/main/scala/lepus/doobie/syntax/ConnectionIOOps.scala
@@ -8,18 +8,15 @@ import scala.annotation.targetName
 
 import lepus.doobie.ConnectionIO
 
-/**
-  * Cats Effect Defines the same extension methods as those defined in IO.
+/** Cats Effect Defines the same extension methods as those defined in IO.
   */
 trait ConnectionIOOps:
 
   extension [T](connection: ConnectionIO[T])
 
-    /**
-      * Runs the current ConnectionIO, then runs the parameter, keeping its result.
-      * The result of the first action is ignored.
-      * If the source fails, the other action won't run.
-      * Evaluation of the parameter is done lazily, making this suitable for recursion.
+    /** Runs the current ConnectionIO, then runs the parameter, keeping its result. The result of the first action is
+      * ignored. If the source fails, the other action won't run. Evaluation of the parameter is done lazily, making
+      * this suitable for recursion.
       */
     @targetName("flatMapAlias1")
     def >>(that: => ConnectionIO[T]): ConnectionIO[T] =
@@ -29,14 +26,12 @@ trait ConnectionIOOps:
     def <<(that: => ConnectionIO[T]): ConnectionIO[T] =
       connection.flatMap(v => that.as(v))
 
-    /**
-      * Replaces the result of this IO with the given value.
+    /** Replaces the result of this IO with the given value.
       */
     def as[B](b: B): ConnectionIO[B] =
       connection.map(_ => b)
 
-    /**
-      * Combination of flatMap and as
+    /** Combination of flatMap and as
       */
     def flatTap[B](f: T => ConnectionIO[B]): ConnectionIO[T] =
       connection.flatMap(a => f(a).as(a))
