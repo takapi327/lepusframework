b37c3364a1827c596214af2bb80502276cb159b4
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-11-27 23:34:46 +0900

    Fix DatabaseModule

--- core/lepus-doobie/src/main/scala/lepus/doobie/DatabaseModule.scala
+++ core/lepus-doobie/src/main/scala/lepus/doobie/DatabaseModule.scala
@@ -4,48 +4,71 @@
 
 package lepus.doobie
 
-import cats.effect.Async
+import scala.concurrent.duration.DurationInt
 
+import com.google.inject.name.Names
+import com.google.inject.AbstractModule
+
+import cats.effect.{ IO, Sync, Resource }
+
+import lepus.guice.module.ResourceModule
 import lepus.database.DatabaseConfig
-import lepus.hikari.HikariContext
+import lepus.hikari.HikariDatabaseBuilder
+import lepus.logger.given
+
+import lepus.doobie.implicits.*
 
 /** Module for implicitly passing the Transactor generated for each Database to DoobieRepository.
   *
-  * @tparam F
-  *   the effect type.
-  *
   * example:
   * {{{
-  *   case class ToDoDatabase(
-  *     database:  DatabaseConfig,
-  *     defaultDB: String
-  *   )(using LepusContext) extends DatabaseModule[F]
-  *
-  *   object ToDoDatabase:
-  *     val db: DatabaseConfig = DatabaseConfig("lepus.database://todo", NonEmptyList.of("master", "slave"))
-  *
-  *     given Transact[ToDoDatabase] = ToDoDatabase(db, "slave")
-  *
-  *     val taskRepository: Transact[TaskRepository] = TaskRepository()
-  *     etc...
+  *   @Singleton
+  *   class ToDoDatabase extends DatabaseModule:
+  *     val databaseConfig: DatabaseConfig = DatabaseConfig("lepus.database://todo/writer")
+  * 
+  *   // Configuration to conf file
+  *   lepus.modules.enabled += "ToDoDatabase"
   * }}}
   */
-trait DatabaseModule[F[_]: Async](using context: HikariContext):
+trait DatabaseModule extends HikariDatabaseBuilder[IO], ResourceModule[ContextIO]:
 
-  /** Value with configuration to establish a connection to Database */
-  protected val database: DatabaseConfig
+  val named: Option[String] = None
 
-  /** Database used by default This value must be contained in the string passed to the replication parameter of
-    * [[DatabaseConfig]]
-    */
-  def defaultDB: String
+  override val resource: Resource[IO, ContextIO] =
+    (for context <- buildContext()
+    yield ContextIO(Transactor.fromDataSource[IO](context.ds, context.ec))).evalTap(v => testConnection(v.xa))
 
-  /** Method to retrieve the Transactor corresponding to the database replication you wish to specify
+  override private[lepus] lazy val build: Resource[cats.effect.IO, AbstractModule] =
+    resource.map(v =>
+      new AbstractModule:
+        override def configure(): Unit =
+          bind(classOf[ContextIO])
+            .annotatedWith(Names.named(named.getOrElse(databaseConfig.named)))
+            .toInstance(v)
+    )
+
+    /** A method that tests the initialized database connection and attempts a wait connection at 5 second intervals until
+    * a connection is available.
     *
-    * TODO: Each time this variable is called, the Transactor would be generated.
+    * @param xa
+    *   A thin wrapper around a source of database connections, an interpreter, and a strategy for running programs,
+    *   parameterized over a target monad M and an arbitrary wrapped value A. Given a stream or program in ConnectionIO
+    *   or a program in Kleisli, a Transactor can discharge the doobie machinery and yield an effectful stream or
+    *   program in M.
     */
-  private[lepus] val transactor: String => Transactor[F] = (key: String) =>
-    database.dataSource
-      .find(_.replication.contains(key))
-      .flatMap(ds => context.get(ds).map(ctx => Transactor.fromDataSource[F](ctx.ds, ctx.ec)))
-      .getOrElse(throw new IllegalStateException(s"$database database is not registered."))
+  def testConnection(xa: Transactor[IO]): IO[Unit] =
+    (testQuery(xa) >> logger.info(s"$databaseConfig Database connection test complete")).onError { (ex: Throwable) =>
+      logger.warn(s"$databaseConfig Database not available, waiting 5 seconds to retry...", ex) >>
+        IO.sleep(5.seconds) >>  testConnection(xa)
+    }
+
+  /** A query to be executed to check the connection to the database.
+    *
+    * @param xa
+    *   A thin wrapper around a source of database connections, an interpreter, and a strategy for running programs,
+    *   parameterized over a target monad M and an arbitrary wrapped value A. Given a stream or program in ConnectionIO
+    *   or a program in Kleisli, a Transactor can discharge the doobie machinery and yield an effectful stream or
+    *   program in M.
+    */
+  def testQuery(xa: Transactor[IO]): IO[Unit] =
+    Sync[IO].void(sql"select 1".query[Int].unique.transact(xa))
