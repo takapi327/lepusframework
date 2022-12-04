95b457fc869d47bd507555472d0f1cf8c4e81db5
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-12-05 00:52:40 +0900

    Action sbt scalafmtALl

--- core/lepus-server/src/main/scala/lepus/server/ClientModule.scala
+++ core/lepus-server/src/main/scala/lepus/server/ClientModule.scala
@@ -1,6 +1,6 @@
 /** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  * file that was distributed with this source code.
+  */
 
 package lepus.server
 
@@ -15,22 +15,21 @@ import org.http4s.ember.client.EmberClientBuilder
 
 import lepus.guice.module.ResourceModule
 
-/**
- * Module for building http4s Ember Client
- * If you want to customize your configuration instead of the default client, you must inherit this module to build it.
- *
- * example:
- * {{{
- *   @Singleton
- *   class CustomClientModule extends ClientModule[IO, Client[IO]]:
- *     protected val resource: Resource[IO, Client[IO]] =
- *       ...custom settings
- *
- *   // setting conf file
- *   lepus.modules.disable += "lepus.server.DefaultClientModule"
- *   lepus.modules.enable  += "path.package.CustomClientModule"
- * }}}
- */
+/** Module for building http4s Ember Client If you want to customize your configuration instead of the default client,
+  * you must inherit this module to build it.
+  *
+  * example:
+  * {{{
+  *   @Singleton
+  *   class CustomClientModule extends ClientModule[IO, Client[IO]]:
+  *     protected val resource: Resource[IO, Client[IO]] =
+  *       ...custom settings
+  *
+  *   // setting conf file
+  *   lepus.modules.disable += "lepus.server.DefaultClientModule"
+  *   lepus.modules.enable  += "path.package.CustomClientModule"
+  * }}}
+  */
 trait ClientModule extends ResourceModule[IO, Client[IO]]:
   protected val resource: Resource[IO, Client[IO]] =
     EmberClientBuilder.default[IO].build
@@ -43,8 +42,7 @@ trait ClientModule extends ResourceModule[IO, Client[IO]]:
             .toInstance(v)
     )
 
-/**
- * Default Http4s client
- */
+/** Default Http4s client
+  */
 @Singleton
 class DefaultClientModule extends ClientModule
