7cf7991ada7b24b81d4a4fadb91f5e366bb10da4
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-12-11 00:49:24 +0900

    Fix LepusServer

--- core/lepus-server/src/main/scala/lepus/server/LepusServer.scala
+++ core/lepus-server/src/main/scala/lepus/server/LepusServer.scala
@@ -29,29 +29,9 @@ import Exception.*
 import lepus.guice.inject.GuiceApplicationBuilder
 import lepus.app.{ LepusApp, BuiltinModule }
 
-private[lepus] object LepusServer extends ResourceApp.Forever, ServerLogging[IO]:
+private[lepus] object LepusServer extends ResourceApp.Forever, ServerBuilder[IO], ServerLogging[IO]:
 
-  private val SERVER_PORT                           = "lepus.server.port"
-  private val SERVER_HOST                           = "lepus.server.host"
-  private val SERVER_ROUTES                         = "lepus.server.routes"
-  private val SERVER_MAX_CONNECTIONS                = "lepus.server.max_connections"
-  private val SERVER_RECEIVE_BUFFER_SIZE            = "lepus.server.receive_buffer_size"
-  private val SERVER_MAX_HEADER_SIZE                = "lepus.server.max_header_size"
-  private val SERVER_REQUEST_HEADER_RECEIVE_TIMEOUT = "lepus.server.request_header_receive_timeout"
-  private val SERVER_IDLE_TIMEOUT                   = "lepus.server.idle_timeout"
-  private val SERVER_SHUTDOWN_TIMEOUT               = "lepus.server.shutdown_timeout"
-
-  private val config: Configuration = Configuration.load()
-
-  private val port:              Int         = config.get[Int](SERVER_PORT)
-  private val host:              String      = config.get[String](SERVER_HOST)
-  private val maxConnections:    Option[Int] = config.get[Option[Int]](SERVER_MAX_CONNECTIONS)
-  private val receiveBufferSize: Option[Int] = config.get[Option[Int]](SERVER_RECEIVE_BUFFER_SIZE)
-  private val maxHeaderSize:     Option[Int] = config.get[Option[Int]](SERVER_MAX_HEADER_SIZE)
-  private val requestHeaderReceiveTimeout: Option[Duration] =
-    config.get[Option[Duration]](SERVER_REQUEST_HEADER_RECEIVE_TIMEOUT)
-  private val idleTimeout:     Option[Duration] = config.get[Option[Duration]](SERVER_IDLE_TIMEOUT)
-  private val shutdownTimeout: Option[Duration] = config.get[Option[Duration]](SERVER_SHUTDOWN_TIMEOUT)
+  private val SERVER_ROUTES = "lepus.server.routes"
 
   def run(args: List[String]): Resource[IO, Unit] =
 
@@ -59,18 +39,13 @@ private[lepus] object LepusServer extends ResourceApp.Forever, ServerLogging[IO]
 
     for
       given Injector <- GuiceApplicationBuilder.build[IO](new BuiltinModule)
-      _              <- buildServer(host, port, lepusApp)
+      _              <- buildServer(lepusApp)
     yield ()
 
-  private def buildServer(
-    host: String,
-    port: Int,
-    app:  LepusApp[IO]
-  )(using Injector): Resource[IO, Server] =
-    EmberServerBuilder
+  def buildServer(app: LepusApp[IO]): Injector ?=> Resource[IO, Server] =
+    var ember = EmberServerBuilder
       .default[IO]
-      .withHost(Ipv4Address.fromString(host).getOrElse(Defaults.host))
-      .withPort(Port.fromInt(port).getOrElse(Defaults.port))
+      .withPort(Port.fromInt(port.getOrElse(Defaults.portInt)).getOrElse(Defaults.port))
       .withHttpApp(app.router)
       .withErrorHandler(app.errorHandler)
       .withMaxConnections(maxConnections.getOrElse(Defaults.maxConnections))
@@ -80,7 +55,14 @@ private[lepus] object LepusServer extends ResourceApp.Forever, ServerLogging[IO]
       .withIdleTimeout(idleTimeout.getOrElse(Defaults.idleTimeout))
       .withShutdownTimeout(shutdownTimeout.getOrElse(Defaults.shutdownTimeout))
       .withLogger(logger.asInstanceOf[Log4catsLogger[IO]])
-      .build
+
+    if enableHttp2.getOrElse(false) then ember = ember.withHttp2
+    else ember                                 = ember.withoutHttp2
+
+    if enableIPv6.nonEmpty then ember = ember.withHost(ipv6Address)
+    else ember                        = ember.withHost(ipv4Address)
+
+    ember.build
 
   private def loadLepusApp(): LepusApp[IO] =
     val routesClassName: String = config.get[String](SERVER_ROUTES)
@@ -103,29 +85,3 @@ private[lepus] object LepusServer extends ResourceApp.Forever, ServerLogging[IO]
           )
 
     constructor
-
-  private object Defaults:
-
-    /** Default host */
-    val host: Host = ipv4"0.0.0.0"
-
-    /** Default port */
-    val port: Port = port"5555"
-
-    /** Default max connections */
-    val maxConnections: Int = 1024
-
-    /** Default receive Buffer Size */
-    val receiveBufferSize: Int = 256 * 1024
-
-    /** Default max size of all headers */
-    val maxHeaderSize: Int = 40 * 1024
-
-    /** Default request Header Receive Timeout */
-    val requestHeaderReceiveTimeout: Duration = 5.seconds
-
-    /** Default Idle Timeout */
-    val idleTimeout: Duration = 60.seconds
-
-    /** The time to wait for a graceful shutdown */
-    val shutdownTimeout: Duration = 30.seconds
