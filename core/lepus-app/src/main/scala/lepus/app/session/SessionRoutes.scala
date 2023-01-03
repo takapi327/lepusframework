0f04d4949f3d55fea6e457e0a7badd6dfbf26574
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-01-03 23:22:55 +0900

    Action sbt scalafmtAll

--- core/lepus-app/src/main/scala/lepus/app/session/SessionRoutes.scala
+++ core/lepus-app/src/main/scala/lepus/app/session/SessionRoutes.scala
@@ -1,6 +1,6 @@
 /** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  * file that was distributed with this source code.
+  */
 
 package lepus.app.session
 
@@ -13,9 +13,8 @@ import org.http4s.*
 /** Type alias for Routes that receives a ContextRequest and returns a ContextResponse. */
 type SessionRoutes[F[_], T] = Kleisli[[A] =>> OptionT[F, A], ContextRequest[F, T], ContextResponse[F, T]]
 
-/**
- * An object for using Session.
- */
+/** An object for using Session.
+  */
 object SessionRoutes:
 
   /** Methods for generating Routes that receive a ContextRequest and return a ContextResponse. */
--- core/lepus-app/src/test/scala/lepus/app/session/SessionMiddlewareTest.scala
+++ core/lepus-app/src/test/scala/lepus/app/session/SessionMiddlewareTest.scala
@@ -1,6 +1,6 @@
 /** This file is part of the Lepus Framework. For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+  * file that was distributed with this source code.
+  */
 
 package lepus.app.session
 
@@ -22,18 +22,20 @@ object SessionMiddlewareTest extends Specification:
   val emptyStorage: IO[SessionStorage[IO, Session]] = SessionStorage.default[IO, Session]()
 
   val sessionRoutes: SessionRoutes[IO, Option[Session]] = SessionRoutes.of[IO, Option[Session]] {
-    case GET -> Root / "session" / "init"   as _       => Ok("Session init").map(resp => ContextResponse(Some(Session("Init")), resp))
-    case GET -> Root / "session" / "reset"  as _       => Ok("Session reset").map(resp => ContextResponse(None, resp))
-    case GET -> Root / "session" / "update" as session => Ok("Session update").map(resp => ContextResponse(session.map(_.copy(name = "Updated")), resp))
+    case GET -> Root / "session" / "init" as _ =>
+      Ok("Session init").map(resp => ContextResponse(Some(Session("Init")), resp))
+    case GET -> Root / "session" / "reset" as _ => Ok("Session reset").map(resp => ContextResponse(None, resp))
+    case GET -> Root / "session" / "update" as session =>
+      Ok("Session update").map(resp => ContextResponse(session.map(_.copy(name = "Updated")), resp))
   }
 
   "Testing the SessionMiddleware" should {
     "If the storage identifier does not exist in the cookie, a new identifier is generated and stored in the response header." in {
       val result = for
         storage <- emptyStorage
-        routes  =  SessionMiddleware.fromConfig[IO, Session](storage)(sessionRoutes)
-        res     <- routes.run(Request().withUri(uri"session/init")).value
-        id      =  selectIdFromResponseHeader(res)
+        routes = SessionMiddleware.fromConfig[IO, Session](storage)(sessionRoutes)
+        res <- routes.run(Request().withUri(uri"session/init")).value
+        id = selectIdFromResponseHeader(res)
         session <- storage.get(SessionIdentifier(id.getOrElse("")))
       yield id.isDefined and session.isDefined and session.map(_.name).contains("Init")
 
@@ -43,11 +45,11 @@ object SessionMiddlewareTest extends Specification:
     "If ContextResponse is set to None with an identifier, the value stored in storage will also be None." in {
       val result = for
         storage <- emptyStorage
-        routes  =  SessionMiddleware.fromConfig[IO, Session](storage)(sessionRoutes)
-        res     <- routes.run(Request().withUri(uri"session/init")).value
-        id      =  selectIdFromResponseHeader(res)
-        old     <- storage.get(SessionIdentifier(id.getOrElse("")))
-        _       <- routes.run(Request().withUri(uri"session/reset").addCookie("LEPUS_SESSION_TEST", id.getOrElse(""))).value
+        routes = SessionMiddleware.fromConfig[IO, Session](storage)(sessionRoutes)
+        res <- routes.run(Request().withUri(uri"session/init")).value
+        id = selectIdFromResponseHeader(res)
+        old <- storage.get(SessionIdentifier(id.getOrElse("")))
+        _   <- routes.run(Request().withUri(uri"session/reset").addCookie("LEPUS_SESSION_TEST", id.getOrElse(""))).value
         session <- storage.get(SessionIdentifier(id.getOrElse("")))
       yield id.isDefined and old.isDefined and old.map(_.name).contains("Init") and session.isEmpty
 
@@ -57,13 +59,15 @@ object SessionMiddlewareTest extends Specification:
     "With the identifier in place, the value stored in storage can be updated." in {
       val result = for
         storage <- emptyStorage
-        routes  =  SessionMiddleware.fromConfig[IO, Session](storage)(sessionRoutes)
-        res     <- routes.run(Request().withUri(uri"session/init")).value
-        id      =  selectIdFromResponseHeader(res)
-        old     <- storage.get(SessionIdentifier(id.getOrElse("")))
-        _       <- routes.run(Request().withUri(uri"session/update").addCookie("LEPUS_SESSION_TEST", id.getOrElse(""))).value
+        routes = SessionMiddleware.fromConfig[IO, Session](storage)(sessionRoutes)
+        res <- routes.run(Request().withUri(uri"session/init")).value
+        id = selectIdFromResponseHeader(res)
+        old <- storage.get(SessionIdentifier(id.getOrElse("")))
+        _ <- routes.run(Request().withUri(uri"session/update").addCookie("LEPUS_SESSION_TEST", id.getOrElse(""))).value
         session <- storage.get(SessionIdentifier(id.getOrElse("")))
-      yield id.isDefined and old.isDefined and old.map(_.name).contains("Init") and session.isDefined and session.map(_.name).contains("Updated") and old != session
+      yield id.isDefined and old.isDefined and old.map(_.name).contains("Init") and session.isDefined and session
+        .map(_.name)
+        .contains("Updated") and old != session
 
       result.unsafeRunSync()
     }
@@ -71,9 +75,9 @@ object SessionMiddlewareTest extends Specification:
     "If a non-existent identifier is used, the value stored in storage cannot be retrieved." in {
       val result = for
         storage <- emptyStorage
-        routes  =  SessionMiddleware.fromConfig[IO, Session](storage)(sessionRoutes)
-        res     <- routes.run(Request().withUri(uri"session/init")).value
-        id      =  selectIdFromResponseHeader(res)
+        routes = SessionMiddleware.fromConfig[IO, Session](storage)(sessionRoutes)
+        res <- routes.run(Request().withUri(uri"session/init")).value
+        id = selectIdFromResponseHeader(res)
         noneOld <- storage.get(SessionIdentifier("hogehoge"))
         success <- storage.get(SessionIdentifier(id.getOrElse("")))
         _       <- routes.run(Request().withUri(uri"session/update").addCookie("LEPUS_SESSION_TEST", "hogehoge")).value
@@ -84,5 +88,10 @@ object SessionMiddlewareTest extends Specification:
     }
   }
 
-  private def selectIdFromResponseHeader(response: Option[Response[IO]], key: String = "LEPUS_SESSION_TEST"): Option[String] =
-    response.flatMap(_.headers.get(CIString("Set-Cookie"))).flatMap(_.head.value.split(';').headOption.map(_.replace(s"$key=", "")))
+  private def selectIdFromResponseHeader(
+    response: Option[Response[IO]],
+    key:      String = "LEPUS_SESSION_TEST"
+  ): Option[String] =
+    response
+      .flatMap(_.headers.get(CIString("Set-Cookie")))
+      .flatMap(_.head.value.split(';').headOption.map(_.replace(s"$key=", "")))
