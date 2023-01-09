27b4646caa59afb2601179d29478ba24e865d270
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-01-09 13:39:02 +0900

    Fix EndpointTest

--- modules/lepus-router/src/test/scala/lepus/router/http/EndpointTest.scala
+++ modules/lepus-router/src/test/scala/lepus/router/http/EndpointTest.scala
@@ -61,7 +61,7 @@ class EndpointTest extends AnyFlatSpec:
 
   it should "The Endpoint string will be the same as the specified string." in {
     val endpoint1: Endpoint[String] = "test1" / bindPath[String]("p1")
-    endpoint1.formatString === "test1/%s"
+    endpoint1.toFormatPath === "test1/%s"
   }
 
   it should "If multiple Endpoints are combined, the string will be the same as the specified value." in {
@@ -69,12 +69,12 @@ class EndpointTest extends AnyFlatSpec:
     val endpoint2: Endpoint[String]           = bindPath[String]("p1") / "test2"
     val endpoint3: Endpoint[(String, String)] = endpoint1 ++ endpoint2
 
-    endpoint3.formatString === "test1/%s/%s/test2"
+    endpoint3.toFormatPath === "test1/%s/%s/test2"
   }
 
   it should "Matches the string specified when the path is generated from Endpoint." in {
     val endpoint: Endpoint[String] = "test1" / bindPath[String]("p1")
-    endpoint.formatString.format("hoge") === "test1/hoge"
+    endpoint.toFormatPath.format("hoge") === "test1/hoge"
   }
 
   it should "Matches the string specified when a path is generated from multiple composited Endpoints." in {
@@ -82,5 +82,25 @@ class EndpointTest extends AnyFlatSpec:
     val endpoint2: Endpoint[Long]           = bindPath[Long]("p1") / "test2"
     val endpoint3: Endpoint[(String, Long)] = endpoint1 ++ endpoint2
 
-    endpoint3.formatString.format("hoge", 1L) === "test1/hoge/1/test2"
+    endpoint3.toFormatPath.format("hoge", 1L) === "test1/hoge/1/test2"
+  }
+
+  it should "The Endpoint string in the Query parameter will be the same as the specified string." in {
+    val endpoint: Endpoint[(Int, Long)] = bindQuery[Int]("page") +& bindQuery[Long]("limit")
+    endpoint.toFormatQuery.format(1, 10L) === "page=1&limit=10"
+  }
+
+  it should "The Endpoint string consisting of multiple Query parameters will be the same as the specified string." in {
+    val endpoint1: Endpoint[List[String]]              = bindQuery[List[String]]("area")
+    val endpoint2: Endpoint[(Int, Long)]               = bindQuery[Int]("page") +& bindQuery[Long]("limit")
+    val endpoint3: Endpoint[(List[String], Int, Long)] = endpoint1 ++ endpoint2
+    endpoint3.toFormatQuery.format("tokyo,kanagawa", 1, 10L) === "area=tokyo,kanagawa&page=1&limit=10"
+  }
+
+  it should "The string of the Endpoint from which the Path and Query are composited will be the same as the specified value." in {
+    val endpoint1: Endpoint[String]              = "hello" / bindPath[String]("p1")
+    val endpoint2: Endpoint[(Int, Long)]         = bindQuery[Int]("page") +& bindQuery[Long]("limit")
+    val endpoint3: Endpoint[(String, Int, Long)] = endpoint1 ++ endpoint2
+
+    endpoint3.formatString.format("world", 1, 10L) === "hello/world?page=1&limit=10"
   }
